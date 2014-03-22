09a459aff4d53bb75f61c432d92744182e151dc1
mschn93 <mschneider93@utexas.edu>
2014-03-22 18:21:22 -0500

    consolidated some macros for the sake of clarity

--- kernel/include/mmap.h
+++ kernel/include/mmap.h
@@ -3,7 +3,7 @@
  */
 
 #define mmio_read(address)		(*(volatile u32_t *)(address))
-#define mmio_write(address, value)	(*(volatile u32_t *)(address = value))
+#define mmio_write(address, value)	(*(volatile u32_t *)(address) = (value))
 
-#define PIC_ADDRESS	0x10140000
-#define UART_ADDRESS	0x101f1000
+#define PIC_ADDRESS	0x10140000	/* interrupt controller peripheral */
+#define UART_ADDRESS	0x101f1000	/* UART 0 base address */
--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -1,6 +1,12 @@
 /*
  *
+ * Interrupts  
+ *
  */
 
-// lol nothing here
-// go look in /include
+/* Return a 32-bit copy of the Current Process Status Register */
+inline unsigned uint32_t get_proc_status(void) {
+	uint32_t cpsr;
+	asm("mrs %0, cpsr" : "=r"(cpsr));
+	return cpsr;
+} 
