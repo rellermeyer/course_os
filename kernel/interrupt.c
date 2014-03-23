5ffddbf929a3f1aad47e930e47be0d2d47a69a8c
Brandon Gottesman <brandon.gottesman@gmail.com>
2014-03-22 22:37:35 -0500

    nothing

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -11,6 +11,6 @@
 /* Return a 32-bit copy of the Current Process Status Register */
 inline uint32_t get_proc_status(void) {
 	uint32_t cpsr;
-	//asm("mrs %0, cpsr" : "=r"(cpsr));
+	asm("mrs %0, cpsr" : "=r"(cpsr));
 	return cpsr;
 } 
