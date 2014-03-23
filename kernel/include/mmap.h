4ab14b84b8215961442be6992625b798d8a1b21d
mschn93 <mschneider93@utexas.edu>
2014-03-22 21:57:21 -0500

    fix missing keyword to eliminate compiler warning

--- kernel/include/mmap.h
+++ kernel/include/mmap.h
@@ -5,8 +5,8 @@
 #ifndef __MMAP_H__
 #define __MMAP_H__
 
-#define mmio_read(address)		(*(volatile u32_t *)(address))
-#define mmio_write(address, value)	(*(volatile u32_t *)(address) = (value))
+#define mmio_read(address)		(*(volatile uint32_t *)(address))
+#define mmio_write(address, value)	(*(volatile uint32_t *)(address) = (volatile uint32_t)(value))
 
 #define PIC_ADDRESS	(volatile uint32_t *const) 0x10140000	/* interrupt controller peripheral */
 #define UART_ADDRESS	(volatile uint32_t *const) 0x101f1000	/* UART 0 base address */
