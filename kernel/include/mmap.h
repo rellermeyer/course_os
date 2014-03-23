305b73987434ebc2fca8625ccecc9925c786dacf
mschn93 <mschneider93@utexas.edu>
2014-03-22 19:07:23 -0500

    macro cleanup

--- kernel/include/mmap.h
+++ kernel/include/mmap.h
@@ -2,8 +2,13 @@
  *  Memory Mapped I/O addresses 
  */
 
+#ifndef __MMAP_H__
+#define __MMAP_H__
+
 #define mmio_read(address)		(*(volatile u32_t *)(address))
 #define mmio_write(address, value)	(*(volatile u32_t *)(address) = (value))
 
-#define PIC_ADDRESS	0x10140000	/* interrupt controller peripheral */
-#define UART_ADDRESS	0x101f1000	/* UART 0 base address */
+#define PIC_ADDRESS	(volatile uint32_t *const) 0x10140000	/* interrupt controller peripheral */
+#define UART_ADDRESS	(volatile uint32_t *const) 0x101f1000	/* UART 0 base address */
+
+#endif // __MMAP_H_
