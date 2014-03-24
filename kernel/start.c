b53463a4abbb01d0bc0722718e6c1c4a1a31009e
Jeffrey Tang <jeffreyftang@gmail.com>
2014-03-24 10:33:23 -0500

    Read SVC call number

--- kernel/start.c
+++ kernel/start.c
@@ -3,31 +3,42 @@
 static const uint32_t LDR_PC_PC = 0xe59ff000;
 
 void test_handler() {
-    print_uart0("success!\n");
+    int i, callNumber;
+
+    // the link register currently holds the address of the instruction immediately
+    // after the SVC call
+    register int address asm("lr"); 
+    
+    // load the SVC call and mask to get the number
+    callNumber = *((uint32_t *)(address-4)) & 0x00FFFFFF;
+
+    for (i = 0; i < callNumber; i++) {
+        print_uart0("success!\n");
+    }
 }
 
 void start() {
-   print_uart0("CourseOS!\n");
+    print_uart0("CourseOS!\n");
 
-   /* Setup primary vector table */
-   *(uint32_t volatile *)(0x00) = (LDR_PC_PC | 0x18);
-   *(uint32_t volatile *)(0x04) = (LDR_PC_PC | 0x18);
-   *(uint32_t volatile *)(0x08) = (LDR_PC_PC | 0x18);
-   *(uint32_t volatile *)(0x0C) = (LDR_PC_PC | 0x18);
-   *(uint32_t volatile *)(0x10) = (LDR_PC_PC | 0x18);
-   *(uint32_t volatile *)(0x14) = (LDR_PC_PC | 0x18);
-   *(uint32_t volatile *)(0x18) = (LDR_PC_PC | 0x18);
-   *(uint32_t volatile *)(0x1C) = (LDR_PC_PC | 0x18);
+    /* Setup primary vector table */
+    *(uint32_t volatile *)(0x00) = (LDR_PC_PC | 0x18);
+    *(uint32_t volatile *)(0x04) = (LDR_PC_PC | 0x18);
+    *(uint32_t volatile *)(0x08) = (LDR_PC_PC | 0x18);
+    *(uint32_t volatile *)(0x0C) = (LDR_PC_PC | 0x18);
+    *(uint32_t volatile *)(0x10) = (LDR_PC_PC | 0x18);
+    *(uint32_t volatile *)(0x14) = (LDR_PC_PC | 0x18);
+    *(uint32_t volatile *)(0x18) = (LDR_PC_PC | 0x18);
+    *(uint32_t volatile *)(0x1C) = (LDR_PC_PC | 0x18);
 
-   /* Setup secondary vector table */
-   *(uint32_t volatile *)(0x20) = (uint32_t)test_handler;
-   *(uint32_t volatile *)(0x24) = (uint32_t)test_handler;
-   *(uint32_t volatile *)(0x28) = (uint32_t)test_handler;
-   *(uint32_t volatile *)(0x2C) = (uint32_t)test_handler;
-   *(uint32_t volatile *)(0x30) = (uint32_t)test_handler;
-   *(uint32_t volatile *)(0x34) = (uint32_t)test_handler;
-   *(uint32_t volatile *)(0x38) = (uint32_t)test_handler;
-   *(uint32_t volatile *)(0x3C) = (uint32_t)test_handler;
+    /* Setup secondary vector table */
+    *(uint32_t volatile *)(0x20) = (uint32_t)test_handler;
+    *(uint32_t volatile *)(0x24) = (uint32_t)test_handler;
+    *(uint32_t volatile *)(0x28) = (uint32_t)test_handler;
+    *(uint32_t volatile *)(0x2C) = (uint32_t)test_handler;
+    *(uint32_t volatile *)(0x30) = (uint32_t)test_handler;
+    *(uint32_t volatile *)(0x34) = (uint32_t)test_handler;
+    *(uint32_t volatile *)(0x38) = (uint32_t)test_handler;
+    *(uint32_t volatile *)(0x3C) = (uint32_t)test_handler;
 
 }
 
