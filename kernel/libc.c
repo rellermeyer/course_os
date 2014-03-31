b338b648e6036909fdd16565f334dd273e59bf50
Jared McArthur <jaredmc@cs.utexas.edu>
2014-03-31 13:17:46 -0500

    fixed block commenting

--- kernel/libc.c
+++ kernel/libc.c
@@ -43,77 +43,77 @@ int os_strcmp ( const char *left, const char *right)
 /* TODO: create print function for kernel debugging purposes */
 
 /*
- *void print_number_using_base(int num, int base, int letter_case, int use_sign){
- *  int temp = num;
- *  int count_digits = 0;
- *  char buf[100];
- *  if (temp == 0){
- *    print_uart0('0');
- *  } else if (use_sign && temp < 0) {
- *    print_uart0('-');
- *    temp = -temp;
- *  }
- *  while(temp != 0){
- *    if(letter_case == UPPER_CASE){
- *      buf[count_digits] = upper_case_digits[(unsigned long) temp % (unsigned) base];
- *    } else {
- *      buf[count_digits] = lower_case_digits[(unsigned long) temp % (signed) base];
- *    }
- *    temp = ((unsigned long) temp / (unsigned) base);
- *    count_digits++;
- *  }
- *  while(count_digits > 0){
- *    char tmp_str[2] = "";
- *    tmp_str[0] = buf[count_digits-1]; 
- *    tmp_str[1] = '\0';
- *    print_uart0(tmp_str);
- *    count_digits--;
- *  }
- *}
- */
+void print_number_using_base(int num, int base, int letter_case, int use_sign){
+  int temp = num;
+  int count_digits = 0;
+  char buf[100];
+  if (temp == 0){
+    print_uart0('0');
+  } else if (use_sign && temp < 0) {
+    print_uart0('-');
+    temp = -temp;
+  }
+  while(temp != 0){
+    if(letter_case == UPPER_CASE){
+      buf[count_digits] = upper_case_digits[(unsigned long) temp % (unsigned) base];
+    } else {
+      buf[count_digits] = lower_case_digits[(unsigned long) temp % (signed) base];
+    }
+    temp = ((unsigned long) temp / (unsigned) base);
+    count_digits++;
+  }
+  while(count_digits > 0){
+    char tmp_str[2] = "";
+    tmp_str[0] = buf[count_digits-1]; 
+    tmp_str[1] = '\0';
+    print_uart0(tmp_str);
+    count_digits--;
+  }
+}
 
-/*
- *int os_printf(const char *str_buf, ...) {
- *  va_list args;
- *  int t_arg;
- *  va_start(args, str_buf);
- *  int count_args_found = 0;
- *  while(*str_buf != '\0') {
- *    if(*str_buf == '%') {
- *      str_buf++;
- *      switch (*str_buf) {
- *        case 'X':
- *          print_number_using_base(va_arg(args, int), 16, UPPER_CASE, 0);
- *          break;
- *        case 'x':
- *          t_arg = va_arg(args,int);
- *          print_number_using_base(t_arg, 16, LOWER_CASE, 0);
- *          break;
- *        case 'd':
- *          print_number_using_base(va_arg(args, int), 10, NO_CASE, 1);
- *          break;
- *        case 'c':
- *          t_arg = va_arg(args,int);
- *          char tmp_str[2] = "";
- *          tmp_str[0] = t_arg; 
- *          tmp_str[1] = '\0';
- *          print_uart0(tmp_str);
- *          break;
- *        case '%':
- *          print_uart0("%");
- *          break;
- *      }
- *    } else {
- *      char tmp_str[2] = "";
- *      tmp_str[0] = (uint32_t) *str_buf; 
- *      tmp_str[1] = '\0';
- *      print_uart0(tmp_str);
- *    }
- *    str_buf++;
- *  }
- *  va_end(args);
- *}
- */
+
+
+int os_printf(const char *str_buf, ...) {
+  va_list args;
+  int t_arg;
+  va_start(args, str_buf);
+  int count_args_found = 0;
+  while(*str_buf != '\0') {
+    if(*str_buf == '%') {
+      str_buf++;
+      switch (*str_buf) {
+        case 'X':
+          print_number_using_base(va_arg(args, int), 16, UPPER_CASE, 0);
+          break;
+        case 'x':
+          t_arg = va_arg(args,int);
+          print_number_using_base(t_arg, 16, LOWER_CASE, 0);
+          break;
+        case 'd':
+          print_number_using_base(va_arg(args, int), 10, NO_CASE, 1);
+          break;
+        case 'c':
+          t_arg = va_arg(args,int);
+          char tmp_str[2] = "";
+          tmp_str[0] = t_arg; 
+          tmp_str[1] = '\0';
+          print_uart0(tmp_str);
+          break;
+        case '%':
+          print_uart0("%");
+          break;
+      }
+    } else {
+      char tmp_str[2] = "";
+      tmp_str[0] = (uint32_t) *str_buf; 
+      tmp_str[1] = '\0';
+      print_uart0(tmp_str);
+    }
+    str_buf++;
+  }
+  va_end(args);
+}
+*/
 
 int main()
 { return 0; }
