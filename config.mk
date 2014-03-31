b22e413585ae390af6a2ee07177b8f878895f8ac
jared <jared@Apophis.(none)>
2014-03-31 00:18:23 -0500

    reverted config.mk to default

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ TOOLCHAIN_DIR=toolchain
 BARE_METAL_TUPLE=arm-none-eabi
 BARE_METAL_TARGET:=$(BARE_METAL_TUPLE)
 
-QEMU=~/os/course_os/qemu/qemu/bin/qemu-system-arm
+QEMU=qemu-system-arm
 
 UBOOT_VERSION=2010.03
