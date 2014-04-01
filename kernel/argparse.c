e45c425972fd5cbb34b2b58d80a7e9782713ef51
Sheldon <sheldon@sandbekkhaug.net>
2014-03-31 23:28:16 -0500

    Started work on reading ATAG_CMDLINE
    Started function to read tags in argparse.c
    Code to call that function from start.c

--- kernel/argparse.c
+++ kernel/argparse.c
@@ -1,10 +1,12 @@
+#include <stdint.h>
+
+
 #include "include/argparse.h"
 
 
 /* Parse the list of strings (argv) and process each argument */
 void parse_arguments(int argc, char **argv)
 {
-  // TODO: free original argv?
   char **argument_list = argv;
 
   int i = 0;
@@ -41,4 +43,38 @@ int analyze_args(char **argv)
 
   // Default case: read one argument
   return 1;
+}
+
+
+void copy_cmdline_tag(uint32_t *tag_base)
+{
+  uint32_t *tag_pointer = tag_base;
+  print_uart0("In copy_cmdline_tag\n"); // TODO: remove
+
+  uint32_t ATAG_CMDLINE = 0x54410009; // Value of cmdline tag will be this
+
+  // Look through the headers starting at tag_base
+  uint32_t size = 0;
+  uint32_t type = 0;
+  while (type != ATAG_CMDLINE)
+  {
+    size = *tag_pointer;
+    tag_pointer += 1;
+    type = *tag_pointer;
+    tag_pointer -= 1;
+
+    if (*tag_pointer == 0x54410002)
+      print_uart0("CORE");
+
+    print_uart0(size);
+    print_uart0(type);
+    tag_pointer += size;
+  }
+
+  // TODO: Get the ATAG_CMDLINE
+  char *command_line_args = tag_pointer;
+  command_line_args = 'a';
+  print_uart0(command_line_args);
+
+  print_uart0("\nDone with copy_cmdline_tag\n"); // TODO: remove
 }
