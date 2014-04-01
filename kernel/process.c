d3ab0ba352a102fe9e7897aac6a0523d621457e0
faseeh akhter <fa4295@cs.utexas.edu>
2014-03-31 20:19:58 -0500

    Initial initialization of processes
    added process_create
    added pcb_table
    added traversal of pcb_table for free slots

--- kernel/process.c
+++ kernel/process.c
@@ -1,5 +1,36 @@
 #include "include/process.h"
 
-int main(){
+//process memory resides starting at 0X20000 
+
+int main() {
 	return 0;
+}
+
+int init_all_processes() {
+	pcb_table = mem_alloc(32);
+} 
+
+int process_create(uint32_t starting_address, char * process_name) {
+	uint32_t* location_in_pcb_table = next_free_slot_in_pcb_table();
+	if(location_in_pcb_table != 0) {
+		int * pcb_pointer = mem_alloc(sizeof(pcb));
+		*location_in_pcb_table = pcb_pointer;
+	}
+	else {
+		print_uart0("Out of memory in pcb table");
+		return -1;
+	}
+
+
+}
+
+uint32_t * next_free_slot_in_pcb_table() {
+	uint32_t* current_address = pcb_table;
+	uint32_t i;
+	for(i = 0; i < MAX_PROCESSES; i++) {
+		current_address += i * 8;
+		if((current_address) == 0)
+			return current_address;
+	}
+	return (uint32_t *) -1;
 }
