ca6b8e493f7397f4f95732b4bba8e08a67b7c544
mpanoratra <smithleet@yahoo.com>
2014-04-02 01:00:27 -0500

    print f works, call using os_printf
    now everyone can start debugging

--- kernel/start.c
+++ kernel/start.c
@@ -3,5 +3,9 @@ void start(void *p_bootargs) {
    print_uart0(44 + p_bootargs);
    print_uart0("\n");
    print_uart0("CourseOS!\n");
+   os_printf("test %d %d %d %d\n", 7, 4, 42, -123);
+   os_printf("test %x %x %x %x\n", 7, 4, 42, -123);
+   os_printf("test %X %X %X %X\n", 7, 4, 42, -123);
+
 }
 
