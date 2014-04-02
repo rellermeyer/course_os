26b3bf9ad31e37bd57b8b1e6375aa50fb6b4e14b
Michael Brennan <mrb3296@cs.utexas.edu>
2014-04-02 18:10:29 -0500

    Added print functionality, and made simple template

--- kernel/uart.c
+++ kernel/uart.c
@@ -1,7 +1,12 @@
 #include <stdint.h>
 
 typedef volatile struct {
+void *driver_stuff;
+} device_driver;
+
+typedef volatile struct {
 /* Register info: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0183g/I18381.html */
+device_driver *dd;
  uint32_t UARTDR; 		// Data Register
  uint32_t UARTRSR_UARTECR; 	// Receive Status Register/Error Clear Register
  uint8_t reserved1[0x10];	// Reserved Space
@@ -18,29 +23,15 @@ typedef volatile struct {
  const uint32_t UARTMIS; 	// Masked Interrupt Status Register
  uint32_t UARTICR; 		// Interrupt Clear Register
  uint32_t UARTDMACR; 		// DMA Control Register
-} pl011_T;
- 
-enum {
-/* UARTFR Reg. Info:  http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0183g/I28167.html */
- RXFE = 0x10,
- TXFF = 0x20,
-};
- 
-pl011_T * const UART0 = (pl011_T *)0x101f1000;
-pl011_T * const UART1 = (pl011_T *)0x101f2000;
-pl011_T * const UART2 = (pl011_T *)0x101f3000;
- 
-static inline char upperchar(char c) {
- if((c >= 'a') && (c <= 'z')) {
-  return c - 'a' + 'A';
- } else {
-  return c;
- }
-}
- 
-static void uart_echo(pl011_T *uart) {
- if ((uart->UARTFR & RXFE) == 0) {
-  while(uart->UARTFR & TXFF);
-  uart->UARTDR = upperchar(uart->UARTDR);
- }
+} uart;
+
+uart * const UART0 = (pl011_T *)0x101f1000;
+uart * const UART1 = (pl011_T *)0x101f2000;
+uart * const UART2 = (pl011_T *)0x101f3000;
+
+void print_uart0(const char *s) {
+	while(*s != '\0') {
+		*UART0 = (uint32_t)(*s);
+		s++;
+	}
 }
