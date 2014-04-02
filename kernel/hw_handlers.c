c20c1ee96fbfc2aac5a5614baae340b932bb44d1
Matthew Schneider <mschneider93@utexas.edu>
2014-04-01 23:16:24 -0500

    moved Jeff's vector table into the new framework

--- kernel/hw_handlers.c
+++ kernel/hw_handlers.c
@@ -1,42 +1,62 @@
-#include "include/interrupt.h"
-
 /*
  *
  * Harware Handler Interface  
  *
  */
 
+void init_vector_table(void) {
+
+	/* Primary Vector Table */
+	mmio_write(0x00, BRANCH_INSTRUCTION);
+	mmio_write(0x04, BRANCH_INSTRUCTION);
+	mmio_write(0x08, BRANCH_INSTRUCTION);
+	mmio_write(0x0C, BRANCH_INSTRUCTION);
+	mmio_write(0x10, BRANCH_INSTRUCTION);
+	mmio_write(0x14, BRANCH_INSTRUCTION);
+	mmio_write(0x18, BRANCH_INSTRUCTION);
+	mmio_write(0x1C, BRANCH_INSTRUCTION);
+
+	/* Secondary Vector Table */
+	mmio_write(0x20, reset_handler);
+	mmio_write(0x24, undef_instruction_handler);
+	mmio_write(0x28, software_interrupt_handler);
+	mmio_write(0x2C, prefetch_abort_handler);
+	mmio_write(0x30, data_abort_handler);
+	mmio_write(0x34, reserved_handler);
+	mmio_write(0x38, irq_handler);
+	mmio_write(0x3C, fiq_handler);
+}
+
 
 /* handlers */
 void reset_handler(void){
-	print_uart0("RESET\n");
+	print_uart0("RESET HANDLER\n");
 }
 
 void undef_instruction_handler(void){
+	print_uart0("UNDEFINED INSTRUCTION HANDLER\n");
 }
 
 void software_interrupt_handler(void){
+	print_uart0("SOFTWARE INTERRUPT HANDLER\n");
 }
 
 void prefetch_abort_handler(void){
+	print_uart0("PREFETCH ABORT HANDLER\n");
 }
 
 void data_abort_handler(void){
+	print_uart0("DATA ABORT HANDLER\n");
 }
 
 void reserved_handler(void){
+	print_uart0("RESERVED HANDLER\n");
 }
 
 void irq_handler(void){
+	print_uart0("IRQ HANDLER\n");
 }
 
 void fiq_handler(void){
-}
-
-
-/* Return a 32-bit copy of the Current Process Status Register */
-inline uint32_t get_proc_status(void) {
-	uint32_t cpsr;
-	asm("mrs %0, cpsr" : "=r"(cpsr));
-	return cpsr;
+	print_uart0("FIQ HANDLER\n");
 }
