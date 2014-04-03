0415386be0d25e0a62640c114b42dedd79d1dc5e
Jeremy Wenzel <wenzeljeremy15@gmail.com>
2014-04-02 19:27:10 -0500

    Elfparser can now parse entire program table

--- kernel/elf.c
+++ kernel/elf.c
@@ -8,8 +8,8 @@
 unsigned char* filePointer;
 
 /* Gets value of bytes given a size of bytes */
-int do_big_endian(int size) {
-	unsigned int value = 0;
+uint32_t do_big_endian(uint32_t size) {
+	uint32_t value = 0;
 	while(size > 0) {
 		value = value << 8;
 		value = value + *filePointer;
@@ -19,9 +19,9 @@ int do_big_endian(int size) {
 	return value;
 }
 
-int do_little_endian(int size) {
-	unsigned int value = 0;
-	unsigned int num = 0;
+uint32_t do_little_endian(uint32_t size) {
+	uint32_t value = 0;
+	uint32_t num = 0;
 	int i = 0;
 	while(i < size) {
 		num = *filePointer;
@@ -34,17 +34,15 @@ int do_little_endian(int size) {
 
 }
 
-int get_value(int size, Elf_Ehdr h)
+uint32_t get_value(uint32_t size, Elf_Ehdr h)
 {
-	unsigned int ret;
 	if(h.e_ident[EI_DATA] == 1)
-		ret = do_little_endian(size);
+		return do_little_endian(size);
 	else
-		ret = do_big_endian(size);
-	return ret;
+		return do_big_endian(size);
 }
 
-int isElf(Elf_Ehdr h) {
+int32_t isElf(Elf_Ehdr h) {
 	// checks if elf file
 	char ELFMAG0 = get_value(1, h);	// 0x7f
 	char ELFMAG1 = get_value(1, h);	// 'E'
@@ -59,120 +57,114 @@ int isElf(Elf_Ehdr h) {
 }
 
 Elf_Ehdr read_elf_header(Elf_Ehdr h) {
-	int check = isElf(h);
-	if(check == -1)
+	int32_t check = isElf(h);
+	int i = 0;
+	if(check == -1){
 		printf("File is not an ELF file.\n");
 		return h;
+	}
 	
 	h.e_ident[EI_CLASS] = get_value(1, h);		// get class
-
 	h.e_ident[EI_DATA] = get_value(1, h);		// endian
 	h.e_ident[EI_VERSION] = get_value(1, h);	// original version of ELF
 	h.e_ident[EI_OSABI] = get_value(1, h);		// Target operating system ABI
 	h.e_ident[EI_ABIVERSION] = get_value(1, h); 	// Don't really know?
-	int skip = 0;					// skips the padding
+	
+	
+	int32_t skip = 0;				// skips the padding
 	while(skip < 7) {
 		get_value(1, h);
 		skip++;
 	}
-	h.e_type = get_value(2, h);			// get type of file
+	
 
+	h.e_type = get_value(2, h);			// get type of file
 	h.e_machine = get_value(2, h);
 	h.e_version = get_value(4, h);
-
 	
 	if(h.e_ident[EI_CLASS] == 1)			// get entry point
 		h.e_entry = get_value(4, h);
 	else
 		h.e_entry = get_value(8, h);
-
 	
 	if(h.e_ident[EI_CLASS] == 1)			// get program header offset
 		h.e_phoff = get_value(4, h);
 	else
 		h.e_phoff = get_value(8, h);
-
 	
 	if(h.e_ident[EI_CLASS] == 1)			// get section header offset
 		h.e_shoff = get_value(4, h);
 	else
 		h.e_shoff = get_value(8, h);
 
-
 	h.e_flags = get_value(4, h);			// get flag number
-	
-
 	h.e_ehsize = get_value(2, h);			// get elf header size
-
-
 	h.e_phentsize = get_value(2, h);		// get program header size
-
-
 	h.e_phnum = get_value(2, h);			// number of program headers
-
-	// Number of Program Headers
-	printf("Number of Program Headers: %i\n", h.e_phnum);
-
 	h.e_shentsize = get_value(2, h);		// section header size
-
-
 	h.e_shnum = get_value(2, h);			// number of section headers
-
-
 	h.e_shstrndx = get_value(2, h);			// section header string table index
 
-return h;
+	print_elf_header(h);
+	while(i < h.e_phnum) {
+		read_program_header_table(h);
+		i++;
+	}
+	
+	return h;
 }
 
 /* Don't use this unless we have printf functionality */
 void print_elf_header(Elf_Ehdr h) {
+
+	printf("ELF Header:\n");
 	// class
 	if (h.e_ident[EI_CLASS] == 1)
-		printf("Class: 32-bit objects\n");
+		printf("ELF32\n");
 	else if (h.e_ident[EI_CLASS] == 2)
-		printf("Class: 64-bit objects\n");
+		printf("ELF64\n");
 	else //invalid
-		printf("Invalid class: %i.\n", h.e_ident[EI_CLASS]);
+		printf("Invalid class:			 %i\n", h.e_ident[EI_CLASS]);
 	
 	// data/endianness
 	if (h.e_ident[EI_DATA] == 1)
-		printf("Data Format: Little Endian\n");
+		printf("Data Format: 				 Little Endian\n");
 	else if (h.e_ident[EI_DATA] == 2)
-		printf("Data Format: Big Endian\n");
+		printf("Data Format: 				 Big Endian\n");
 	else //invalid
-		printf("Invalid data encoding: %i.\n", h.e_ident[EI_DATA]);
+		printf("Invalid data encoding:		 %i.\n", h.e_ident[EI_DATA]);
 
 	// versio
 	if (h.e_ident[EI_VERSION] == 1)
-		printf("Version: Currrent version\n");
+		printf("Version: 				 Currrent version\n");
 	else	//invalid
-		printf("Invalid version: %i.\n", h.e_ident[EI_VERSION]);
+		printf("Invalid version:		 %i.\n", h.e_ident[EI_VERSION]);
 
 	// OS ABI
 	if (h.e_ident[EI_OSABI] == 0)
-		printf("OS-specific flags: None\n");
+		printf("OS-specific flags: 			 None\n");
 	else	//??? not sure what to do otherwise
-		printf("OS ABI value: %i\n", h.e_ident[EI_OSABI]);
+		printf("OS ABI value:			 %i\n", h.e_ident[EI_OSABI]);
 
 	// ABI version
 	if (h.e_ident[EI_ABIVERSION] != 0)
-		printf("Invalid ABI version: %i.\n", h.e_ident[EI_ABIVERSION]);
+		printf("Invalid ABI version:		 %i.\n", h.e_ident[EI_ABIVERSION]);
 	
 	// File Type
 	if (h.e_type == 1)
-		printf("File type: Relocatable\n");
+		printf("File type:			 	 Relocatable\n");
 	else if (h.e_type == 2)
-		printf("File type: Executable\n");
+		printf("File type:			 	 Executable\n");
 	else if (h.e_type == 3)
-		printf("File type: Shared Object\n");
+		printf("File type:				 Shared Object\n");
 	else if (h.e_type == 4)
-		printf("File type: Core\n");
+		printf("File type:			 	 Core\n");
 	else
-		printf("Invalid file type: %i\n", h.e_type);
+		printf("Invalid file type:		 	 %i\n", h.e_type);
 
 	// Machine type
 	if (h.e_machine == 40)
-		printf("Machine architecture: ARM\n");
+		printf("Machine architecture:			 ARM\n");
 	else	
 		printf("Invalid machine architecture.\n");
 
@@ -181,13 +173,13 @@ void print_elf_header(Elf_Ehdr h) {
 		printf("Invalid version.\n");
 
 	// Entry point
-	printf("Entry point: %i\n", h.e_entry);
+	printf("Entry point:				 0x%08X\n", h.e_entry);
 
 	// Program Header Offset
-	printf("Program Header Offset: %i\n", h.e_phoff);
+	printf("Program Header Offset:			 %i\n", h.e_phoff);
 
 	// Section Header Offset
-	printf("Section Header Offset: %i\n", h.e_shoff);
+	printf("Section Header Offset:			 %i\n", h.e_shoff);
 
 	// Flags
 	if (h.e_flags == EF_ARM_ABIMASK)
@@ -204,25 +196,76 @@ void print_elf_header(Elf_Ehdr h) {
 		printf("No header flags set.\n");
 	
 	// Elf Header Size
-	printf("Elf Header Size: %i\n", h.e_ehsize);
+	printf("Elf Header Size:			 %i\n", h.e_ehsize);
 
 	// Program Header Size
-	printf("Program Header Size: %i\n", h.e_phentsize);
+	printf("Program Header Size:			 %i\n", h.e_phentsize);
 
 	// Number of Program Headers
-	printf("Number of Program Headers: %i\n", h.e_phnum);
+	printf("Number of Program Headers:		 %i\n", h.e_phnum);
 
 	// Section Header Size
-	printf("Section Header Size: %i\n", h.e_shentsize);
+	printf("Section Header Size:			 %i\n", h.e_shentsize);
 
 	// Number of Section Headers
-	printf("Number of Section Headers: %i\n", h.e_shnum);
+	printf("Number of Section Headers:		 %i\n", h.e_shnum);
 
 	// Section Header String Table Index
-	printf("Section Header String Table Index: %i\n", h.e_shstrndx);
+	printf("Section Header String Table Index:	 %i\n", h.e_shstrndx);
 
 }
 
+void print_program_header_table(Elf_Phdr ph) {
+	printf("--------------------------------------\n");
+
+	printf("Type: ");
+	if(ph.p_type ==	PT_NULL)
+		printf("NULL\n");
+	else if(ph.p_type == PT_LOAD)
+		printf("LOAD\n");
+	else if(ph.p_type == PT_DYNAMIC)
+		printf("DYNAMIC\n");
+	else if(ph.p_type == PT_INTERP)
+		printf("INTERP\n");
+	else if(ph.p_type == PT_NOTE)
+		printf("NOTE\n");
+	else if(ph.p_type == PT_SHLIB)
+		printf("SHLIB\n");
+	else if(ph.p_type == PT_PHDR)
+		printf("PHDR\n");
+	else
+		printf("Hdr not found... yet\n");
+
+	
+	printf("Offset: 	 	 0x%08X\n", ph.p_offset);
+	printf("Virtual Address:	 0x%08X\n", ph.p_vaddr);
+	printf("Physical address:	 0x%08X\n", ph.p_paddr);
+	printf("File Size:		 0x%08X\n", ph.p_filesz);
+	printf("MemSiz:			 0x%08X\n", ph.p_memsz);
+
+	printf("Flags: 			 0x%08X\n", ph.p_flags);
+
+	printf("Alignment:		 0x%08X\n", ph.p_align);
+	printf("--------------------------------------\n");
+}	
+
+void read_program_header_table(Elf_Ehdr eh) {
+	Elf_Phdr ph;
+
+	ph.p_type = get_value(4, eh);
+	ph.p_offset = get_value(4, eh);
+	ph.p_vaddr = get_value(4, eh);
+	ph.p_paddr = get_value(4, eh);
+	ph.p_filesz = get_value(4, eh);
+	ph.p_memsz = get_value(4, eh);
+	
+	ph.p_flags = get_value(4, eh);
+
+	ph.p_align = get_value(4, eh);
+	
+
+	print_program_header_table(ph);
+}
 void read_section_header_table(int32_t fd, Elf_Ehdr h, Elf_Shdr sh_table[]) {
 	// reads section header
 }
@@ -290,7 +333,8 @@ int main() {
 	file_contents = filePointer = read_whole_file("a.out");
 	Elf_Ehdr e;
 	//read_elf_header(e);
-	print_elf_header(read_elf_header(e));
+	e = read_elf_header(e);
+	//print_elf_header(e);
 	free(file_contents);
 	return 0;
 }
