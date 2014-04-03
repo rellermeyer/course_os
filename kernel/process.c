1139f55d478df373163612725d07e5fec3bd2117
stv8 <stvillars8@gmail.com>
2014-04-02 20:07:05 -0500

    Initial Process allcation

--- kernel/process.c
+++ kernel/process.c
@@ -1,17 +1,27 @@
 #include "include/process.h"
+#include "include/klibc.h"
 
 //process memory resides starting at 0X20000 
 
 
 int init_all_processes() {
-	pcb_table = mem_alloc(32);
+	pcb_table = mem_alloc(MAX_PROCESSES);
+	
+	//DEBUG
+	//os_printf("%x\n", pcb_table);
+	GLOBAL_PID = 0;
 } 
 
-int process_create(uint32_t starting_address, char * process_name) {
+int process_create(uint32_t starting_address, char* process_name) {
 	uint32_t* location_in_pcb_table = next_free_slot_in_pcb_table();
+	
 	if(location_in_pcb_table != 0) {
 		int * pcb_pointer = mem_alloc(sizeof(pcb));
 		*location_in_pcb_table = pcb_pointer;
+
+		pcb* new_pcb;
+		new_pcb->name = process_name;
+		new_pcb->PID = ++GLOBAL_PID;
 	}
 	else {
 		print_uart0("Out of memory in pcb table");
