4b56ef1cb6ef9e4fedb041ccfa3aefbd0131b3b0
alek0313 <alek.anwar@utexas.edu>
2014-04-06 00:33:43 -0500

    mapped kernel code and data; turned on mmu

--- kernel/initvm.c
+++ kernel/initvm.c
@@ -1,8 +1,7 @@
 /* Virtual memory layout
  *
  * 0x00000000 - 0x7fffffff (0-2GB) = user process memory
- * 0x80000000 - 0xa0ffffff (2GB) = physical memory
- * 	includes peripherals at 0x20000000 - 0x20ffffff
+ * 0x80000000 - 0x88000000 = physical memory (128M)
  * 0xc0000000 - 0xefffffff = kernel heap/stack
  * 0xf0000000 - 0xffffffff = kernel code
  *
@@ -13,6 +12,9 @@
 extern unsigned int _kstart, _kend;
 extern unsigned int _datastart, _dataend;
 extern unsigned int _bssstart, _bssend;
+
+//Call start when done
+extern void start(void*);
 
 // 4096 entries = 16KB below kernel
 static unsigned int *levelone_pt = (unsigned int * const)0xc000; 
@@ -63,9 +65,60 @@ void initvm(void){
 		}
 	}
 
-	//Map the first MB into VM at hr same location.
+	//Map the first MB into VM at the same location.
 	//This is where the code is currently running
 	levelone_pt[0] = 0<<20 | 0x0400 | 2;
 
+
+	/*
+	 * Reserved 1MB for kernel image in physical memory
+	 * which inlcudes the initvm, start, code, data and bss
+	 * Map this MB using a section at 0xf0000000 - 0xf00fffff
+	 * which corresponds to virtual page number 3840
+	 * 	-- map as read-only
+	 *  ?? leave 0xf00fffff - 0xffffffff if kernel grows
+	 *	    -- if so, will need more coarse tables 
+     *
+	*/
+    levelone_pt[3840] = 0<<20 | 0x8400 | 2;
+
+    /* 0xc0000000-0xc00fffff is mapped to physical memory by a coarse
+	 * page table
+	 *
+	 * This maps the kernel data from where it has been loaded in memory
+	 * (ie somewhere after initvm and start) to 0xc0000000
+	 * At the moment this is the only kernel structure in the kernel heap region
+	 */
+	initpagetable[3072] = 1 | (unsigned int)kernel_leveltwo_pt;
+
+
+	//TODO: initailze the 256 entries in the coarse table
+
+
+	//Initialize .bss section
+	unsigned int* bss;
+	bss = &_bssstart;
+	while(bss < &_bssend){
+		*bss = 0;
+		bss++;
+	}
+
+	//Set TTBR0 and TTBR1 in the MMU with the address
+	//of the level one page table
+	unsigned int pt_addr = (unsigned int)levelone_pt;
+
+	// Translation table 0 - ARM1176JZF-S manual, 3-57
+	asm volatile("mcr p15, 0, %[addr], c2, c0, 0" : : [addr] "r" (pt_addr));
+	// Translation table 1 
+	asm volatile("mcr p15, 0, %[addr], c2, c0, 1" : : [addr] "r" (pt_addr));
+
+	//TODO: other MMU registers
+
+	//Set lr to the address of start
+	asm volatile("mov lr, %[start]" : : [start] "r" ((unsigned int)&start) );
+	//Restore registers used in start
+	asm volatile("pop {r0, r1, r2}");
+	//Jump to value in lr
+	asm volatile("bx lr");
 
 }
