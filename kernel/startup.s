670a81047c64d7350e10b8b8527be2870d9e74ae
alek0313 <alek.anwar@utexas.edu>
2014-04-05 23:35:36 -0500

    set sp for different proccesor modes

--- kernel/startup.s
+++ kernel/startup.s
@@ -1,7 +1,19 @@
 bal _Reset
 
 _Reset:
- LDR sp, =stack_top
+
+ CPS #0x1f
+ LDR sp, =system_stack
+
+ CPS #0x12
+ LDR sp, =irq_stack
+
+ CPS #0x17
+ LDR sp, =abort_stack
+
+ CPS #0x13
+ LDR sp, =svc_stack
+
  MOV R0, R2
- BL start
+ BL initsys
  WFI
