100972a7443baea8ab780d96fe4c78a3b8e8a1f0
stv8 <stvillars8@gmail.com>
2014-04-07 19:22:57 -0500

    fixed bump pointer allocation to allocate by word
    continue to pass in byte value and it will return words

--- kernel/include/mem_alloc.h
+++ kernel/include/mem_alloc.h
@@ -1,6 +1,7 @@
 /*
 Log
 4/2: Adjusted bump allocation algorithm: Sean V, Faseeh A, John G, Taylor S
+4/7: Fixed mem_alloc again works properly: Sean V, Faseeh A, Taylor S.
 
 */
 
--- kernel/memory/mem_alloc.c
+++ kernel/memory/mem_alloc.c
@@ -6,11 +6,14 @@ static const uint32_t MEM_START = 0x500000;
 uint32_t *nextBlock = 0x500000;  
 
 //bump pointer allocation
-uint32_t * mem_alloc(uint32_t size) {
+uint32_t* mem_alloc(uint32_t size) {
+	uint32_t temp = size / 4;
+
+	if((size % 4) > 0) {
+		temp++;
+	}
 	uint32_t* allocBlock = nextBlock;
-    nextBlock = nextBlock + size * 8;
+    nextBlock = nextBlock + size;
    
-   	//Debug
-    //os_printf("%d\n", nextBlock);
     return allocBlock;
 }
