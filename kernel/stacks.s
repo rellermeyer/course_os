ae20e10700330937c903d3bfd1c1dfce61cb70ff
alek0313 <alek.anwar@utexas.edu>
2014-04-13 23:37:25 -0500

    seperate assembly file for stack setup

---
+++ kernel/stacks.s
@@ -0,0 +1,47 @@
+.equ stack_size, 0x1000
+.equ stack_base, 0xf0000000
+
+.equ Mode_USR, 0x10
+.equ Mode_FIQ, 0x11
+.equ Mode_IRQ, 0x12
+.equ Mode_SVC, 0x13
+.equ Mode_MON, 0x16
+.equ Mode_ABT, 0x17
+.equ Mode_UND, 0x1B
+.equ Mode_SYS, 0x1F
+
+.globl _stacks
+
+_stacks:
+
+ LDR     R0, =stack_base
+ MSR     CPSR_c, #Mode_FIQ
+ MOV     sp, R0
+ SUB     R0, R0, #stack_size
+
+ MSR     CPSR_c, #Mode_IRQ
+ MOV     sp, R0
+ SUB     R0, R0, #stack_size
+
+ MSR     CPSR_c, #Mode_SVC
+ MOV     sp, R0
+ SUB     R0, R0, #stack_size
+
+ MSR     CPSR_c, #Mode_MON
+ MOV     sp, R0
+ SUB     R0, R0, #stack_size
+
+ MSR     CPSR_c, #Mode_ABT
+ MOV     sp, R0
+ SUB     R0, R0, #stack_size
+
+ MSR     CPSR_c, #Mode_UND
+ MOV     sp, R0
+ SUB     R0, R0, #stack_size
+
+ MSR     CPSR_c, #Mode_SYS
+ MOV     sp, R0
+
+ MSR     CPSR_c, #Mode_SVC
+
+ B 		 main
--- kernel/start.c
+++ kernel/start.c
@@ -1,25 +1,32 @@
 #include "include/global_defs.h"
 #include <stdint.h>
 #include "include/argparse.h"
+#include "include/mmap.h"
+
+//extern void setup_stacks(void);
 
 void start(void *p_bootargs) {
    print_uart0("arguments: ");
    print_uart0(read_cmdline_tag(p_bootargs));
    print_uart0("\n");
    print_uart0("CourseOS!\n");
+   
    mmap();
 
-   //Test: UART0 mapped to the correct virtual address
+   //Test: UART0 mapped to the correct virtual address   
    print_vuart0("Virtual Memory!!!\n");
 
-   //TODO: more test to verify layout
-   //test_vmlayout();
+   //setup new stack pointers
+   //setup_stacks();
+   asm volatile("b _stacks");
 
-   //Unmap where the kernel was originally loaded
-   unmap();
+   /*
+   //flush TLB
+   asm volatile(
+      "eor r0, r0 \n\t"
+      "MCR p15, 0, r0, c8, c7, 0 \n\t");
+   */
 
-   //returns and waits for interrupt in startup.s
-   //uncomment below to wait here instead
-   //asm volatile("wfi");
 }
+
 
