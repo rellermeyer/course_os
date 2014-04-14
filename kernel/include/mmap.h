8eeca4d207141e6fe6172f0eb9ec5fdf711b97f2
alek0313 <alek.anwar@utexas.edu>
2014-04-13 22:33:29 -0500

    added physical memory header and minor change to vm header

--- kernel/include/mmap.h
+++ kernel/include/mmap.h
@@ -5,12 +5,11 @@
 #ifndef __MMAP_H__
 #define __MMAP_H__
 
+
 #define mmio_read(address)		(*((volatile uint32_t *)(address)))
 #define mmio_write(address, value)	(*((volatile uint32_t *)(address)) = (volatile uint32_t)(value))
 
 #define PIC_ADDRESS	(volatile uint32_t *const) 0x10140000	/* interrupt controller peripheral */
 #define UART_ADDRESS	(volatile uint32_t *const) 0x101f1000	/* UART 0 base address */
-
-static unsigned int* first_level_pt = (unsigned int* const)0x700000;
 
 #endif // __MMAP_H_
--- kernel/include/vmlayout.h
+++ kernel/include/vmlayout.h
@@ -5,31 +5,29 @@
  *
  *
  *			+-----------------------+ 	0xffff ffff
- *			|vectors & other kernel |
- *			|branch locations	|	(?? 1MB)
- *			|  _________________ 	|	0xffef ffff
- *			|			|
- *			|	KERNEL 		|	(255 MB)
+ *			|						|
+ *          :                       :
+ *			|		kernel 			|	(255 MB)
  *			+-----------------------+	0xf000 0000 			
- *			|6 kernel stacks w/	|
- *			|6 invalid mem in btw	|	(12*4K=48K)
- *			+-----------------------+	0xefff ffff
- *			|			|
- *			|	reserved for	|
- *			:	kernel data	:	(1038 MB)
- *			:	structures	:
- *			|			|
+ *			|7 kernel stacks w/		|
+ *			|7 invalid mem in btw	|	(14*4K=56K)
+ *			+-----------------------+	
+ *			|						|
+ *			|	reserved for		|
+ *			:	kernel data			:	(1038 MB)
+ *			:	structures			:
+ *			|						|
  *			+-----------------------+	0xaf20 0000
- *			|	 PCI Interface	|	(752MB)
+ *			|	 PCI Interface		|	(752MB)
  *			+-----------------------+	0x8020 0000
  *			|Peripheral Registers	|	
- *			|	(UART etc)	|	(2MB)
+ *			|	(UART etc)			|	(2MB)
  *			+-----------------------+	0x8000 0000
- *			|			|
- *			|			|
- *			:	User Space	:	(2GB)
- *			:			:
- *			|			|
+ *			|						|
+ *			|						|
+ *			:	User Space			:	(2GB)
+ *			:						:
+ *			|						|
  *			|_______________________|	0x0000 0000
  */
 
@@ -44,12 +42,15 @@
 #define KSTACKTOP		KERNSTART
 #define	KSTACKSIZE		0x1000	//size of a stack
 #define KSTACKGAP		0x1000	//size of stack guard
+#define V_KDSBASE			0xeff00000
+#define	V_L2PTREGBASE	0xefb00000
 
 #define PCISTART		0x80200000
+#define PCIEND			0xaf200000
 
 #define PREGSTART		0x80000000
 
-#define USER_LIM		CREGSTART
+#define USER_LIM		PREGSTART
 
 //Use this macro to get virtual address of peripherals
 #define peri_p2v(pa)		(pa+PREGSTART)
