c88a8b2e46e5098e1c891045ddceaf740682883f
Sean Villars <stvillars8@gmail.com>
2014-04-15 11:47:15 -0500

    fixed errors

--- kernel/loader.c
+++ kernel/loader.c
@@ -3,7 +3,7 @@
 #include "include/klibc.h"
 #include "include/process.h"
 #include <stdint.h>
-
+#include "include/elf.h"
 #define USER_PROC_STACK_SIZE 0x100000 //1 MB
 #define KERNEL_PROC_STACK_SIZE 0x1000 //4k
 
@@ -16,16 +16,16 @@ void det_mem_block_size(Elf_Ehdr *h, Elf_Shdr *sh[], uint32_t * file_pointer)
 //Take a process control block and pointer to the start of an ELF file in memory.
 void load_file(pcb * process_control_block, uint32_t * file_pointer)
 {
-	Elf_Ehdr *h = (Elf_Ehdr *)mem_alloc(sizeof(Elf_ehdr));
+	Elf_Ehdr *h = (Elf_Ehdr *)mem_alloc(sizeof(Elf_Ehdr));
 	read_elf_header(*h, file_pointer);
 	
 	Elf_Phdr ph[h->e_phnum];
-	read_program_header_table(h, ph);
+	read_program_header_table(*h, ph, file_pointer);
 
 	Elf_Shdr sh[h->e_shnum];
-	read_section_header_table(h, sh);
+	read_section_header_table(*h, sh, file_pointer);
 	
-	parse_section_header_names(h, sh, file_pointer);	
+	parse_section_header_names(*h, sh, file_pointer);	
 }
 
 //this will allocate memory for the entire process
