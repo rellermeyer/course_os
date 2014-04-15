0ca549e9453fe8dd2b340ac2599c0942d1232220
Jeffrey Tang <jtang@cs.utexas.edu>
2014-04-15 13:56:05 -0500

    Continue reworking saving/restoring protocol

--- user/ulibc.s
+++ user/ulibc.s
@@ -2,9 +2,10 @@
 .global yield
 .global printf
 
+; Not sure, but we might not need to ever return from here
+; since a different process will now run
 exit:
     SVC 0
-    MOV pc, lr 
 
 ; Need to save registers on the process stack
 ; since we can't rely on kernel to do it
@@ -14,10 +15,13 @@ yield:
     LDR r1, sp      ; pass sp to handler
                     ; these will be saved in PCB
     SVC 1
+
+printf:
+    PUSH {r0-r12,lr} ; r7 will be overwritten by SVC call,
+                     ; so we might as well save here...
+    SVC 2
+    B resume
+
 resume:
     POP {r0-r12,pc} ; jump through lr to caller
 
-; Working state will be saved/restored by kernel
-printf:
-    SVC 2
-    MOV pc, lr
