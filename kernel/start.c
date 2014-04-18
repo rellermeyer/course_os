a654e22bc68de9aa942b55c44179a2e028a13468
Matthew Schneider <mschneider93@utexas.edu>
2014-04-17 20:08:44 -0500

    accidentally deleted start.c from my last commit

---
+++ kernel/start.c
@@ -0,0 +1,36 @@
+/*
+ *  A bit of background:
+ *  - The ARM architecture has 7 modes of operation:
+ *      + USR - user mode
+ *      + FIQ - processing "fast" interrupts
+ *      + IRQ - processing "normal" interrupts
+ *      + SVC - proctected mode for OS
+ *      + UND - processing an undefined instruction exception
+ *      + SYS - also protecteed mode for OS --if anyone wants to clarify, feel free--
+ *
+ *  - These modes can be entered or exited by modifying the CPSR (status register), first 5 bits
+ *	+ 0b10000 = user mode
+ *	+ 0b10001 = FIQ (fast interrupt) mode
+ *	+ 0b10010 = IRQ (normal interrupt) mode
+ *	+ 0b10011 = SVC (supervisor, or, OS) mode
+ *	(others...)		
+ */
+#include <stdint.h>
+#include "hw_handlers.h"
+#include "global_defs.h"
+#include "argparse.h"
+
+
+void start(void *p_bootargs) {
+   print_uart0("CourseOS!\n");
+   print_uart0("arguments: ");
+   print_uart0(read_cmdline_tag(p_bootargs));
+   print_uart0("\n");
+
+    /* we boot into SVC mode with FIQ and IRQ masked */
+    /* TODO: intialize the vector table, stack space, etc. */
+	md((uint32_t *)0x00);
+    init_vector_table();
+	md((uint32_t *)0x00);
+	asm volatile("SWI 7");	
+}
