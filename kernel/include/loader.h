2f75c5b35e21822ac75f3d8e8f5535ffb306a6a8
KaelenHaag1 <khaag@utexas.edu>
2014-04-18 12:28:16 -0500

    Worked on the memory_allocation part of the loader will work on it more tonight

--- kernel/include/loader.h
+++ kernel/include/loader.h
@@ -4,6 +4,9 @@
 #include "elf.h"
 #include "process.h"
 
+#define USER_PROC_STACK_SIZE 0x100000 //1 MB
+#define KERNEL_PROC_STACK_SIZE 0x1000 //4K
+
 //typedef enum{TEXT, RODATA, DATA, RODATA, BSS, SYMTBL} Section;
 
 void load_file(pcb *, uint32_t *); //Needs to be of the type that's where ever our beginning of file is in mem
