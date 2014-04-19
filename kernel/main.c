0c6b801bb60d3c52a1a98bb0f141a64f31e042c5
alek0313 <alek.anwar@utexas.edu>
2014-04-18 23:21:00 -0500

    debugging: entry addr shouldn't need offset -> debug return from mem_alloc

--- kernel/main.c
+++ kernel/main.c
@@ -2,6 +2,7 @@
 #include "include/pmap.h"
 #include "include/vmlayout.h"
 #include "include/klibc.h"
+#include "include/mem_alloc.h"
 #include <stdint.h>
 
 extern void* bootargs;
@@ -25,27 +26,34 @@ void main(void){
   v_printf("MAIN1: pc=%x, lr=%x, sp=%x, fp=%x\n", pc, lr, sp, fp);  
 
   //Unmap one-to-one kernel and pt mappings
-  *(first_level_pt+KERNSTART+(KERNDSBASE>>20)) = 0;   
-  *(first_level_pt+KERNSTART) = 0;
+  //*(v_first_level_pt+(KERNDSBASE>>20)) = 0;   
+  //*(v_first_level_pt) = 0;
 
-  asm volatile("mov %0, pc" : "=r" (pc));
-  asm volatile("mov %0, lr" : "=r" (lr));
-  asm volatile("mov %0, sp" : "=r" (sp));
-  asm volatile("mov %0, fp" : "=r" (fp));
-  v_printf("MAIN1: pc=%x, lr=%x, sp=%x, fp=%x\n", pc, lr, sp, fp);
+  unsigned int entry_addr = (v_first_level_pt);   
+  v_printf("entry_addr=%x\n", entry_addr);
+
+  unsigned int entry_addr_off = (v_first_level_pt + KERNSTART);   
+  v_printf("entry_addr_off=%x\n", entry_addr_off);  
 
 
-  /* Allocate in kernel region using k_malloc()
-   * Allocate in user region using u_malloc()
-   */
-  uint32_t * test = k_malloc(sizeof(uint32_t*));
-  if(test == 0){
-    v_printf("mem_alloc error\n");
-  }
-  v_printf("testaddr=%x\n", test);
-  *test = 0x33333;
-  //initialize GLOBAL_PID and PCB table
-  init_all_processes();
+  // asm volatile("mov %0, pc" : "=r" (pc));
+  // asm volatile("mov %0, lr" : "=r" (lr));
+  // asm volatile("mov %0, sp" : "=r" (sp));
+  // asm volatile("mov %0, fp" : "=r" (fp));
+  // v_printf("MAIN1: pc=%x, lr=%x, sp=%x, fp=%x\n", pc, lr, sp, fp);
+
+
+  //  Allocate in kernel region using k_malloc()
+  //  * Allocate in user region using u_malloc()
+   
+  // uint32_t * test = mem_alloc(sizeof(uint32_t*), KERN);
+  // if(test == 0){
+  //   v_printf("mem_alloc error\n");
+  // }
+  // v_printf("testaddr=%x\n", test);
+  // *test = 0x33333;
+  // //initialize GLOBAL_PID and PCB table
+  // init_all_processes();
 
   asm volatile("wfi");
 
