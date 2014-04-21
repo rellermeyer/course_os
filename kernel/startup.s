206876403a53ae92c568485fcebde3d163eed387
Matthew Schneider <mschneider93@utexas.edu>
2014-04-21 12:49:28 -0500

    changed vector table remapping process to read from a .text section in startup.s rather than a #define'd macro instruction (better flexibility)

--- kernel/startup.s
+++ kernel/startup.s
@@ -1,3 +1,6 @@
+.text
+	.code 32
+
 .equ stack_size, 256
 .equ stack_base, 0x20FFF8
 
@@ -10,7 +13,30 @@
 .equ Mode_UND, 0x1B
 .equ Mode_SYS, 0x1F
 
-bal _Reset
+	.global	vector_table_start
+	.global vector_table_end
+	
+	/* exception vector table */
+	/* will be copied over to 0x00 upon reset */
+	vector_table_start:
+		LDR	PC, reset_handler_addr
+		LDR	PC, undef_instruction_handler_addr
+		LDR	PC, software_interrupt_handler_addr
+		LDR	PC, prefetch_abort_handler_addr
+		LDR	PC, data_abort_handler_addr
+		LDR	PC, reserved_handler_addr
+		LDR	PC, irq_handler_addr
+		LDR	PC, fiq_handler_addr
+
+		reset_handler_addr:			.word	_Reset
+		undef_instruction_handler_addr:		.word	undef_instruction_handler
+		software_interrupt_handler_addr:	.word	software_interrupt_handler
+		prefetch_abort_handler_addr:		.word	prefetch_abort_handler
+		data_abort_handler_addr:		.word	data_abort_handler
+		reserved_handler_addr:			.word	reserved_handler
+		irq_handler_addr:			.word	irq_handler
+		fiq_handler_addr:			.word	fiq_handler
+	vector_table_end:
 
 _Reset:
  @ LDR sp, =stack_top
