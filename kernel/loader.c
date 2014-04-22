35b63d7c15f3c4d6ce01660643ec63e03042a9ab
KaelenHaag1 <khaag@utexas.edu>
2014-04-21 19:01:24 -0500

    Adding segments into memory location

--- kernel/loader.c
+++ kernel/loader.c
@@ -4,8 +4,6 @@
 #include "include/process.h"
 #include <stdint.h>
 #include "include/elf.h"
-//#define USER_PROC_STACK_SIZE 0x100000 //1 MB
-//#define KERNEL_PROC_STACK_SIZE 0x1000 //4k
 
 //We determine what the size of our process is going to be
 //Determine the size of the segments that need to be loaded in (has p_type == PT_LOAD)
@@ -35,12 +33,22 @@ size_t det_proc_size(Elf_Ehdr *h, Elf_Phdr ph[])
 	
 }
 
-
-void allocate_process_memory(pcb *pcb_p, Elf_Ehdr *h, Elf_Phdr ph[])
+//Temporary add this to somewhere relevant (klibc)
+void memcpy(uint32_t * source, uint32_t * dest, size_t size)
 {
-	size_t process_size = det_proc_size(h, ph);
-	uint32_t * process_mem = mem_alloc(process_size);
-	
+	size_t i = 0;
+	for(; i < size; i++)
+	{
+		*(source + size) = *(dest + size);
+	}
+}
+
+
+void allocate_process_memory(pcb *pcb_p, Elf_Ehdr *h, Elf_Phdr ph[], uint32_t * file_pointer)
+{
+	 size_t process_size = det_proc_size(h, ph);
+	uint32_t * process_mem = u_malloc(process_size);
+	uint32_t * current_pointer = process_mem;
 	//We're gonna copy each segment into memory at a position. Calculate what needs to done to change
 	//the entry_point to account for where we've placed something in memory
 	
@@ -66,8 +74,22 @@ void allocate_process_memory(pcb *pcb_p, Elf_Ehdr *h, Elf_Phdr ph[])
 	*      |  RO   |
 	*      |  Seg  |
 	*      | ***** | <- process_mem	
-	*/ 
+	*/
+	//Puts all the relevant segments into allocated region
+	int i = 0;
+	for(; i < (h->e_phnum); i++)
+        {
+		if(ph[i].p_type == PT_LOAD)
+		{
+			memcpy(file_pointer + ph[i].p_offset, current_pointer, ph[i].p_memsz);
+			current_pointer = current_pointer + ph[i].p_memsz;
+		}
+	}
+
 	
+	
+			
+		
 }
 
 //Probably want to return a memory address rather than nothing.
@@ -80,10 +102,10 @@ void load_file(pcb * process_control_block, uint32_t * file_pointer)
 	Elf_Phdr ph[h->e_phnum];
 	read_program_header_table(*h, ph, file_pointer);
 
-	Elf_Shdr sh[h->e_shnum];
-	read_section_header_table(*h, sh, file_pointer);
+	//Elf_Shdr sh[h->e_shnum];
+	//read_section_header_table(*h, sh, file_pointer);
 	
-	parse_section_header_names(*h, sh, file_pointer);	
+	//parse_section_header_names(*h, sh, file_pointer);	
 }
 
 //this will allocate memory for the entire process
