bb7e5a236f69a8f720c0dc488ca912ac5e6fcd86
Matthew Schneider <mschneider93@utexas.edu>
2014-04-24 14:24:40 -0500

    fixing issues from that monster merge -- removed unnecssary flag from 'make run' command -- we're going to use a coded vector table and not macros

--- kernel/start.c
+++ kernel/start.c
@@ -30,51 +30,22 @@ void uart_handler(void *null) {
 }
 
 void start(void *p_bootargs) {
-  char *cmdline_args = read_cmdline_tag(p_bootargs);
 
-  print_uart0("arguments: ");
-  print_uart0(cmdline_args);
-  print_uart0("\n");
-  print_uart0("CourseOS!\n");
+	// we're not using Virtual memory here
+	//	mmap();  
+	init_vector_table();
+	asm volatile("SWI 7");
 
-  // Separate the command-line arguments into separate Strings
-  int num_args = number_of_words(cmdline_args);
-  char* arg_list[num_args];
-  split_string(cmdline_args, arg_list);
-  int arg_count = sizeof(arg_list) / sizeof(arg_list[0]);
+	asm volatile (".include \"stacks.s\"");
 
-  // Parse and analyze each String
-  parse_arguments(arg_count, arg_list);
-
-  //don't allow interrpts messing with memory
-  disable_interrupts();
-  //setup page table and enable MMU
-  mmap();  
-  //register handlers
-  init_vector_table();
-  interrupt_handler_t uart0_handler_struct = { &uart_handler };
-  register_interrupt_handler(UART0_IRQ, &uart0_handler_struct);
-
-  UART0_IMSC = 1<<4;
-  VIC_INT_ENABLE = 1<<12;
-  enable_interrupts();
-
-  //asm volatile("wfi");
-
-  //Test: UART0 mapped to the correct virtual address   
-  print_vuart0("MMU enabled\n");
-
-  //setup new stack pointers and jump to main
-  asm volatile (".include \"stacks.s\"");
-
-  /* NOTHING EXECUTED BEYOND THIS POINT
-  *
-  *
-  * Anything that needs to be setup right after
-  * booting the kernel should go before mmap()
-  *
-  * Any setup, heap allocation or stack allocation
-  * goes in main
-  *
-  */
+	/* NOTHING EXECUTED BEYOND THIS POINT
+	 *
+	 *
+	 * Anything that needs to be setup right after
+	 * booting the kernel should go before mmap()
+	 *
+	 * Any setup, heap allocation or stack allocation
+	 * goes in main
+	 *
+	 */
 }
--- kernel/startup.s
+++ kernel/startup.s
@@ -1,9 +1,40 @@
-.global _Reset
-bal _Reset
+.text
+.code 32
+
+.equ stack_size, 256
+.equ stack_base, 0x20FFF8
+
+.equ Mode_USR, 0x10
+.equ Mode_FIQ, 0x11
+.equ Mode_IRQ, 0x12
+.equ Mode_SVC, 0x13
+.equ Mode_MON, 0x16
+.equ Mode_ABT, 0x17
+.equ Mode_UND, 0x1B
+.equ Mode_SYS, 0x1F
+
+.global	vector_table_start
+.global vector_table_end
+.global	_Reset
+
+/* exception vector table */
+/* will be copied over to 0x00 upon reset */
+vector_table_start:
+	b	_Reset
+	b	undef_instruction_handler
+	b	software_interrupt_handler
+	b	prefetch_abort_handler
+	b	data_abort_handler
+	b	.
+	b	irq_handler
+	b	fiq_handler
+vector_table_end:
 
 _Reset:
-    LDR sp, =stack_top
-    MOV R0, R2
-    BL start
-    WFI
 
+/* ensure we're in supervisor mode w/ irq/fiq disabled */
+MSR	cpsr_c, #Mode_SVC
+LDR	sp, =stack_top
+BL 	start
+
+WFI
