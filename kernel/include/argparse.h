28a5cdada1a40c5996bc809ae9d845c0a5633ee5
Sheldon <sheldon@sandbekkhaug.net>
2014-04-24 19:28:43 -0500

    Created function to read hex string into an unsigned int
    Wrote string_to_unsigned_int() to convert string to an unsigned int
    Wrote hex_value_of_character() to get the value of a character (not ASCII value)
    Added code to use these functions in analyze_arguments()

--- kernel/include/argparse.h
+++ kernel/include/argparse.h
@@ -5,3 +5,5 @@ int analyze_args(char **argv);
 char* read_cmdline_tag(uint32_t *tag_base);
 char** split_string(char *line, char **list);
 int number_of_words(char *line);
+int string_to_unsigned_int(char *input, int base);
+int hex_value_of_character(char c);
