7c6472a4dbca6ca310a818b5b25431e41a874a8a
Michael Brennan <mrb3296@cs.utexas.edu>
2014-04-27 18:42:58 -0500

    Created basic outline of signal queue

--- kernel/include/signals.h
+++ kernel/include/signals.h
@@ -1,11 +1,15 @@
-//Contributors: Andrew Stepek, Michael Brennen, and Matthew Stromberg
+#ifndef SIGNAL_H
+#define SIGNAL_H
+
+//Contributors: Michael Brennen, Andrew Stepek, and Matthew Stromberg
 
 #include <stdint.h>
-#include "include/mmap.h"
-#include "include/process.h"
+#include "mmap.h"
+#include "process.h"
 
 typedef struct signal
 {
 	char * type;
 } sig;
 
+#endif
--- kernel/signals.c
+++ kernel/signals.c
@@ -1,16 +1,63 @@
 //Contributors: Andrew Stepek, Michael Brennen, and Matthew Stromberg
 
 /*
-Signals
-*/
-/*
-SIGKILL
-SIGUSR
-SIGTERM
+Signals: SIGKILL, SIGUSR, SIGTERM, SIGPRINT
 */
 
 #include "include/signals.h"
-#include "include/process.h"
+#include "include/mem_alloc.h"
+
+struct sig
+{
+	uint32_t signal_type;
+	uint32_t pid;
+	struct sig *next;
+};
+
+struct sig *front = '\0';
+struct sig *rear = '\0';
+
+void insert(struct sig *new_sig)
+{
+	sig struct *temp = malloc(sizeof(struct sig));
+	temp = new_sig;
+	if (isEmpty())
+	{
+		temp->next = '\0';
+		front = rear = temp;
+	} 
+	else
+	{
+		temp->next = '\0';
+		rear->next = temp;
+		rear = temp;
+	}
+}
+
+struct sig *remove()
+{
+	struct sig *temp;
+	temp = front;
+	front = front->next;
+	temp->next = '\0';
+	free(temp);
+	if(!front)
+	{
+		rear = '\0';
+	}
+}
+
+int isEmpty()
+{
+	if (front == '\0')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
 
 void signal_mask (char * type, int index, uint32_t PID)
 {
@@ -18,3 +65,16 @@ void signal_mask (char * type, int index, uint32_t PID)
 	pcb_t.mask[index]->type = type;	
 }
 
+int raise(int sig)
+{
+	// raises the signal
+}
+
+void signal_handler(int sig_no)
+{
+/*
+	switch(sig_no)
+	{
+	}
+*/
+}
