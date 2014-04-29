c5ae6c922ba0c7aa9681888b3fb33af9094064fe
Kevin Hicks <khicks@cs.utexas.edu>
2014-04-28 20:12:04 -0500

    fill queue methods

--- kernel/data_structures/queue.c
+++ kernel/data_structures/queue.c
@@ -25,17 +25,54 @@
  }
 
 int is_empty(queue* queue) {
-  
+  return queue->front == -1;
+}
+
+int is_full(queue* queue) {
+  return (queue->rear == MAX_SIZE - 1 && queue->front == 0) || queue->rear == queue->front - 1;
 }
 
 int size(queue* queue) {
+  return 0;
 }
 
 queue_elem peek(queue* queue) {
+  return queue->data[queue->front];
 }
 
-void push(queue* queue, queue_elem* elem) {
+void enqueue(queue* queue, queue_elem elem) {
+  if (is_full(queue))
+    return;
+
+  if (queue->front == -1) {
+    queue->front++;
+    queue->rear++;
+  }
+  else if (queue->rear == MAX_SIZE-1) {
+    queue->rear = 0;
+  }
+  else {
+    queue->rear++;
+  }
+  queue->data[queue->rear] = elem;
 }
 
-queue_elem pop(queue* queue) {
+queue_elem* dequeue(queue* queue) {
+  if (is_empty(queue))
+    return NULL;
+
+  queue_elem temp = queue->data[queue->front];
+  
+  if (queue->front == queue->rear) {
+    queue->front = -1;
+    queue->rear = -1;
+  }
+  else if (queue->front == MAX_SIZE-1) {
+    queue->front = 0;
+  }
+  else {
+    queue->front++;
+  }
+
+  return &temp;
 }
