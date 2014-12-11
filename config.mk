a6dc2c9f53aced06e43cff74cdc30b1a73458819
OverEcho <overecho@overecho.com>
2014-12-10 19:13:17 -0600

    Bruce sebastine

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ TOOLCHAIN_DIR=toolchain
 BARE_METAL_TUPLE=arm-none-eabi
 BARE_METAL_TARGET:=$(BARE_METAL_TUPLE)
 
-QEMU=qemu-system-arm
+QEMU=../qemu/qemu/bin/qemu-system-arm
 
 UBOOT_VERSION=2010.03
