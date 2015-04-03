97a0002d5dbb171d2b56e43c406d513e75fdc5b3
c4k9c0 <charliecox17@yahoo.com>
2015-04-03 12:38:16 -0500

    Moved bitvector to data_structures

---
+++ kernel/data_structures/bitvector.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+#include <../include/mem_alloc.h>
+#include <../include/bitvector.h>
+
+
+uint64_t *makeVector(uint32_t size) {
+	// make the array 
+	uint64_t bitVec = (uint64_t*) mem_alloc(ceil(size/64)); // Can I use ceil in the kernel?
+	return bitVec;
+}
+
+// toggles a given bit
+uint32_t toggle (uint32_t index) {
+	uint64_t val = floor(index/64); // can I use floor in the kernel?
+	uint64_t onePart = bitVec[val];
+	uint64_t mask = 0x1 << 63 - (index % 64);
+	bitVec[val] = bitVec[val] ^ mask;
+	return 1;
+}
