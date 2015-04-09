ca19427e66f7299143208e1363e8c19e5147eb1e
Ginevra Gaudioso <ginevra@cs.utexas.edu>
2015-04-08 20:21:17 -0500

    changed implementation of opentable under Joe's advice, from LL to array

--- kernel/fs/open_table.c
+++ kernel/fs/open_table.c
@@ -1,65 +1,100 @@
-//this file contains the table of open files, implemented as a double linked list
-//with methods like add_to_opentable and delete_from_opentable to be called in open and close 
+//This file contains the table of open files, implemented as an array
+// with methods like add_to_opentable and delete_from_opentable to be called in open and close 
+//Since methods are provided, other files should not touch neither the free LL or the arrray, 
+// but just use the premade methods. 
 
-#include "file.h"  //add directory if necessary
+#include "../include/file.h" 
+#include "../include/klibc.h"
 
-//only has fd and pointers. More fields can be added if necessary.
-// typedef struct file_descriptor 
-// {
-// 	file* file;
-// 	struct file_descriptor * previous;
-// 	// int fd; //file descriptor
-// 	int offset; //keeps track of where we are reading
-// 	struct file_descriptor * next;	
-// 	int open;
-// } file_descriptor;
-
-// beginning of list 
-file_descriptor * HEAD;
-
-// end of list
-file_descriptor * TAIL;
+//number of files that can fit in the table
+#define SYSTEM_SIZE 512; 
 
 
-//this function can be used to insert a file in the list and returns a pointer to it
-file_descriptor* add_to_opentable(file_descriptor* fd_to_add) 
+// Each cell of the array is a struct with infos about the file.
+// More fields can be added if necessary.
+typedef struct file_descriptor 
 {
-	file_descriptor to_add;
-	to_add->fd = fd_to_add;
+ 	struct file * linked_file; //pointer to file struct 
+ 	int offset; //keeps track of where we are reading
+} file_descriptor;
 
-	if (HEAD==NULL) //empty list
-	{
-		HEAD=to_add;
-		TAIL=to_add;
-	}
+// Array of open files 
+struct file_descriptor table[SYSTEM_SIZE];
 
-	else //add to existing list
-	{
-		TAIL->next=to_add;
-		to_add->previous=TAIL;
-		TAIL=to_add;
-	}
-	return to_add;
+
+// LL of free indexes at which we can place a newly opened file.
+typedef struct free_index {
+	int index;
+	struct free_index * next;
+} free_index;
+
+// FIFO structure. When a file is closed, it is added to the tail. 
+// When requested to open a file, the index is taken from the head. 
+struct free_index * HEAD;
+struct free_index * TAIL;
+
+//called by start.c, will initialize the free list and the table
+void fs_table_init()
+{
+	struct free_index * behind = kmalloc(sizeof(struct free_index));
+        HEAD = behind;
+        int i;
+        int max = SYSTEM_SIZE-1;
+        for (i=0; i<max; i++) {
+                behind->index = i;
+                struct free_index * ahead = kmalloc(sizeof(struct free_index));
+                behind->next = ahead;
+                behind = ahead;
+        } 
+        behind->index = SYSTEM_SIZE;
+        TAIL = behind;
+	//note: this memory should be freed with a cleanup function once the computer turns off
+ 	//since there is no turn off at the moment, the function is still fo be implemented
 }
+
+
+// this function can be used to insert a file in the table
+// and returns the requested index if successful, else -1 
+int add_to_opentable(struct file * f) 
+{
+	if (HEAD==NULL) //not enough space
+		return -1; 	
+	int fd = HEAD->index; //take available fd
+	struct free_index * free_me = HEAD; 
+        HEAD = HEAD->next; //dequeue
+	kfree(free_me); //free old node
+	struct file_descriptor to_add; //initialize the struct
+	to_add.linked_file = f;
+	to_add.offset = 0; 
+	table[fd] = to_add; //add to table ---> hopefully this copies the struct and not only a pointer!
+	return fd;
+}
+
 
 //this function can be used to delete a file from the list
 //returns 0 if all ok, -1 if wrong
-int delete_from_opentable(file_descriptor* fd_to_delete)
+int delete_from_opentable(int fd)
 {
-	if (HEAD==NULL)
-		return -1; //error
-	
-	if (fd_to_delete==HEAD ^ fd_to_delete==TAIL) { 
-		HEAD=NULL;
-		TAIL=NULL;
-	}
-	else if (fd_to_delete==HEAD)
-		HEAD=fd_to_delete->next;
-	else if (fd_to_delete==TAIL)
-		TAIL=fd_to_delete->previous;
-	else {
-		fd_to_delete->previous->next=fd_to_delete->next;
-		fd_to_delete->next->previous=fd_to_delete->previous;
-	}
-	return 0;
+	if (table[fd]==NULL) 
+		return -1; //invalid entry
+	table[fd]=NULL;
+	struct free_index * to_add = kmalloc(sizeof(struct free_index)); //create new node
+	to_add->index = fd;
+	TAIL->next = to_add;
+	TAIL = to_add; //enqueue
+
 }
+
+
+//this function checks whether the file is open or not
+int file_is_open(int fd) 
+{
+	return (table[fd]!=NULL);
+}
+
+
+
+
+
+
+
--- user/lib/fs_syscalls.c
+++ user/lib/fs_syscalls.c
@@ -4,18 +4,18 @@
 #include "../../kernel/include/open_table.h"
 
 
-/*open() returns a file descriptor...which is a file_descriptor pointer
-   to the fd in the open file description table...found in open_table.c*/
-file_descriptor* open(char* filepath, char mode){
+/*open() returns a file descriptor, an int used to call later fuctions.
+A return value of -1 tells that an error occurred and the file cannot be opened*/
+int open(char* filepath, char mode){
 	if(mode != 'r' || mode != 'w' || mode != 'a'){
 		os_printf("mode is not a valid option. must be r, w, or a\n");
 		return -1;
 	}//end if
 
-	file_descriptor* fd;
-	// preserve registers r1, r2, and r3 on the stack
-    asm volatile("push r1" "\n\t"
-                 "push r2" "\n\t");
+	int fd;
+	// preserve registers r1, r2 on the stack
+         asm volatile("push r1" "\n\t"
+                      "push r2" "\n\t");
 
 	// move the arguments into registers for kopen() to use:
 	asm volatile("mov %0, (filepath)": "r1");
@@ -29,35 +29,39 @@ file_descriptor* open(char* filepath, char mode){
 	asm volatile("mov %0, r1" : "=r" (fd));
 
 	// Restore r1, r2, and r3 to original values
-    asm volatile("pop r2" "\n\t"
-                 "pop r1" "\n\t");
+        asm volatile("pop r2" "\n\t"
+                     "pop r1" "\n\t");
+	
 	return fd;
 }//end open syscall
+ 
 
-/*create() returns a file descriptor...which is a file_descriptor pointer
-   to the fd in the open file description table...found in open_table.c*/
-file_descriptor* create(char* filepath){
-	file_descriptor fd;
+
+/*create() returns a file descriptor to use in later functions*/
+int create(char* filepath){
+	int fd;
 	fd = open(filepath, 'w');
 	return fd;
 }//end create syscall
 
-/*read() returns a file descriptor...which is a small non-negative int value
-  that corresponds to the index of the file in the system wide open file
-  description table*/
-int read(file_descriptor* fd, void* buf, int numBytes){
-	if(!fd->open){
-		os_printf("fd does not point to a valid open file\n");
+
+
+/*read() returns the amount of bytes successfully read from the file*/
+int read(int fd, void* buf, int numBytes){
+
+	if(!file_is_open(fd)){
+		os_printf("file not open \n");
 		return -1;
-	}//end if
+	}
 	if (numBytes<=0)
-		os_printf(“Invalid number of bytes \n”);
+		os_printf("Invalid number of bytes \n");
+
 	int bytesRead; //this will be what we return
 
-    // preserve registers r1, r2, and r3 on the stack
-    asm volatile("push r1" "\n\t"
-                 "push r2" "\n\t"
-                 "push r3" "\n\t");
+        // preserve registers r1, r2, and r3 on the stack
+        asm volatile("push r1" "\n\t"
+                     "push r2" "\n\t"
+                     "push r3" "\n\t");
 
 	// move the arguments into registers for kread to use:
 	asm volatile("mov %0, (fd)": "r1");
@@ -72,32 +76,33 @@ int read(file_descriptor* fd, void* buf, int numBytes){
 	asm volatile("mov %0, r1" : "=r" (bytesRead));
 
 	// Restore r1, r2, and r3 to original values
-    asm volatile("pop r3" "\n\t"
+        asm volatile("pop r3" "\n\t"
                  "pop r2" "\n\t"
                  "pop r1" "\n\t");
-	returns bytesRead;
+	return bytesRead;
 	//note, actual data read from file gets moved to buf in kread()
 }//end open syscall
 
+
+
 /*write() returns the number of bytes that were actually written*/
-int write(file_descriptor* fd, void* buf, int numBytes){
-	// Error checking:
-	if(!fd->open){
-		os_printf("fd does not point to a valid open file\n");
+int write(int fd, void* buf, int numBytes){
+
+	if(!file_is_open(fd)){
+		os_printf("file not open \n");
 		return -1;
-	}//end if
-	// more error checking:
+	}
 	if (numBytes<=0){
 		os_printf("Invalid number of bytes\n");
 		return -1;
-	}//end if
+	}
 	
 	int bytesWritten; //this will be what we return
 
 	// preserve registers r1, r2, and r3 on the stack
-    asm volatile("push r1" "\n\t"
-                 "push r2" "\n\t"
-                 "push r3" "\n\t");
+         asm volatile("push r1" "\n\t"
+                      "push r2" "\n\t"
+                      "push r3" "\n\t");
 
 	// move the arguments into registers for kwrite to use:
 	asm volatile("mov %0, (fd)": "r1");
@@ -112,30 +117,29 @@ int write(file_descriptor* fd, void* buf, int numBytes){
 	asm volatile("mov %0, r1" : "=r" (bytesWritten));
 
 	// Restore r1, r2, and r3 to original values
-    asm volatile("pop r3" "\n\t"
-                 "pop r2" "\n\t"
-                 "pop r1" "\n\t");
+         asm volatile("pop r3" "\n\t"
+                      "pop r2" "\n\t"
+                      "pop r1" "\n\t");
 	return bytesWritten;
 	//note, actual data written from file gets moved to buf in kwrite()
 }//end write syscall
 
 
+
 /*close() returns the 0 if successful and -1 if not*/
-int close(file_descriptor* fd){
+int close(int fd){
 	int error; //0 if close ok, -1 if error. This will be return.
-	/* the compiler should  throw an error if a user passes a pointer argument that
-	   is not of the type file_descriptor*/
-	// error checking:
-	if(!fd->open){
-		// file is already closed, so return:
-		return 0;
-	}// no other error checking needed...it is up to the user to pass the right fd pointer
+	
+        if(!file_is_open(fd)){
+                os_printf("file not open \n");
+                return -1;
+        }
 
 	// preserve registers r1, r2, and r3 on the stack  —— need all 3?
-    asm volatile("push r1" "\n\t");
+        asm volatile("push r1" "\n\t");
 
 	// move the arguments into registers for kclose to use:
-	asm volatile("mov %0, (fd)": "r1");//note, <register> not necessarily correct...need to figure out which register to use
+	asm volatile("mov %0, (fd)": "r1");
 
 	// trigger the software_interrupt_handler in hw_handler.c:
 	asm volatile("swi SYSCALL_CLOSE");
@@ -145,32 +149,35 @@ int close(file_descriptor* fd){
 	asm volatile("mov %0, r1" : "=r" (error));
 
 	// Restore r1, r2, and r3 to original values
-    asm volatile("pop r1" "\n\t");
+        asm volatile("pop r1" "\n\t");
+
 	return error;
 }//end close syscall
 
+
+
 /*seek() returns the number of bytes that the file offset is successfully moved
   if successful, and -1 if the numBytes puts puts them outside the file length*/
-int seek(file_descriptor* fd, int numBytes){
-	int error; //-1 if numBytes + current file offset exceeds file length. This will be return.
+int seek(int fd, int numBytes){
+	int error; //-1 if numBytes + offset exceeds file length. This will be return.
 	// Error checking...no need to go into kernel if numBytes is invalid:
 	if(numBytes == 0){
 		return 0;
 	} else if ((numBytes > 0) && ((fd->offset + numBytes) > (fd->file)->size)){
-		os_printf("Error! file offset exceeds file size (numBytes is positive\n");
+		os_printf("Error! file offset exceeds file size \n");
 		return -1;
 	}else if ((numBytes < 0) && ((fd->offset + numBytes) < 0)){
-		os_printf("Error! file offset exceeds beginning of file (numBytes is negative\n");
+		os_printf("Error! file offset exceeds beginning of file \n");
 		return -1;
 	}//end if else
 
-	// preserve registers r1, r2, and r3 on the stack  —— need all 3?
-    asm volatile("push r1" "\n\t"
-                 "push r2" "\n\t");
+	// preserve registers r1, r2 on the stack  —— need all 3?
+        asm volatile("push r1" "\n\t"
+                     "push r2" "\n\t");
 
 	// move the arguments into registers for kseek() to use:
-	asm volatile("mov %0, (fd)": "r1");//note, <register> not necessarily correct...need to figure out which register to use
-	asm volatile("mov %0, (numBytes)": "r2");//note, <register> not necessarily correct...need to figure out which register to use
+	asm volatile("mov %0, (fd)": "r1");
+	asm volatile("mov %0, (numBytes)": "r2");
 
 	// trigger the software_interrupt_handler in hw_handler.c:
 	asm volatile("swi SYSCALL_SEEK");
@@ -179,11 +186,12 @@ int seek(file_descriptor* fd, int numBytes){
 	// retrieve the return values from kread to pass back to user:
 	asm volatile("mov %0, r1" : "=r" (error));
 
-	// Restore r1, r2, and r3 to original values
-    asm volatile("pop r2" "\n\t"
-                 "pop r1" "\n\t");
+	// Restore r1, r2 to original values
+        asm volatile("pop r2" "\n\t"
+                     "pop r1" "\n\t");
 	return error;
 }//end seek syscall
+
 
 
 /*delete() returns the 0 if successful and -1 if not*/
@@ -191,10 +199,10 @@ int delete(char* filepath){
 	int error; //0 if delete ok, -1 if error. This will be return.
 	// error checking:
 	// probably will have to check  if file exists in kdelete, but check here if possible...
-	// preserve registers r1, r2, and r3 on the stack
-    asm volatile("push r1" "\n\t");
+	// preserve registers r1 on the stack
+        asm volatile("push r1" "\n\t");
 
-	// move the arguments into registers for kopen() to use:
+	// move the arguments into registers for kdelete() to use:
 	asm volatile("mov %0, (filepath)": "r1");
 
 	// trigger the software_interrupt_handler in hw_handler.c:
@@ -204,7 +212,12 @@ int delete(char* filepath){
 	// retrieve the return values from kopen to pass back to user:
 	asm volatile("mov %0, r1" : "=r" (error));
 
-	// Restore r1, r2, and r3 to original values
-    asm volatile("pop r1" "\n\t");
+	// Restore r1 to original values
+        asm volatile("pop r1" "\n\t");
 	return error;
 }//end delete syscall
+
+
+
+
+
