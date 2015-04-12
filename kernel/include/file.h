5f0ace6c9beaa419da96a62954318133d0b9b388
wwselleck <wwselleck@gmail.com>
2015-04-12 14:41:22 -0500

    New file.h

---
+++ kernel/include/file.h
@@ -0,0 +1,35 @@
+#ifndef __FILE_H__
+#define __FILE_H__
+
+
+#include "bitvector.h"
+#include "linked_list.h"
+#include <stdint.h>
+
+struct file
+{
+	/* data */
+	char* fname;
+	list inode;
+	uint16_t size;
+	bitvector perms; // a bitvector of length three to track: read, write, execute
+	time creation_time; // need to look up CourseOS specific data type
+	uint16_t offset;
+};
+
+int kopen(char* filepath, char mode);
+int kread(int fd, void* buf, int numBytes);
+int kwrite(int fd, void* buf, int num_bytes);
+int kclose(int fd);
+int kseek(int fd, int num_bytes);
+int kdelete(char* filepath);
+
+#endif //__FILE_H__
+
+
+
+
+
+
+
+
