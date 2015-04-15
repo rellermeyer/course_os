3fa8bca4c5656b645eee6932dd741ea17cdf65c6
Joel Iventosch <joeliven@cs.utexas.edu>
2015-04-15 18:57:50 -0500

    updates to kread() in file.c Joel and Charlie; G look at our notes on open_table.c

--- kernel/fs/fat16/file.c
+++ kernel/fs/fat16/file.c
@@ -3,6 +3,7 @@
 #include "../../include/bitvector.h"
 #include "../../include/open_table.h"
 #include "../../include/mmci.h" //TO BE MADE
+#include "../../include/klibc.h"
 
 //NOTES:
 //free list = keeps track of which blocks on the storage device are free (i.e. available)...must be stored persistently
@@ -23,11 +24,6 @@ struct file
 	time creation_time; // need to look up CourseOS specific data type
 };
 
-// typedef struct inode
-// {
-// 	 data 
-
-// } inode;
 
 int kopen(char* filepath, char mode){
 	int fd;
@@ -71,141 +67,134 @@ int kopen(char* filepath, char mode){
 }//end kopen()
 
 
+// Helper function for kread():
+int read_partial_block(int bytesLeft, void* buf_offset, file_descriptor* fd, void* transferSpace) {
+	int local_offset = fd->offset % BLOCKSIZE; // local_offset is the leftmost point in the block
+
+	// Actually get the data for 1 block (the SD Driver will put it in transferSpace for us)
+	int blockNum = fd->offset / BLOCKSIZE;
+	int success = recieve(transferSpace, blockNum);
+	if(success < 0){
+	 	// failed on a block receive, therefore the whole kread fails; return failure error
+	 	os_printf("failed to receive block number %d\n", numBytes);
+	 	return -1;
+	}//end if
+
+	if((local_offset == 0) && (bytesLeft < BLOCKSIZE)) { 
+	/*	 ___________________
+		|~~~~~~~~~|			|
+		--------------------- */
+		// Actually move the data to the user's specified buffer...must first cast void pointers to uint32_t* pointers:
+		// source is transferSpace
+		// dest is users buffer
+
+		 os_memcpy(transferSpace, buf_offset, (os_size_t) bytesLeft); 	// note, this updates the buf_offset pointer as it transfer the data
+		 																// os_memcpy takes uint32_t* as arguments
+		 fd->offset += bytesLeft; // update the file descriptors file offset
+		 // reset transferSpace pointer
+		 transferSpace -= bytesLeft;
+		 return bytesLeft; // note, we are returning the number of bytes that were successfully transferred
+
+	} else if((local_offset > 0) && (bytesLeft >= (BLOCKSIZE - local_offset)) {
+	/*	_____________________
+		|           |~~~~~~~~|
+		---------------------- */
+		// Actually move the data to the user's specified buffer...must first cast void pointers to uint32_t* pointers:
+		// source is transferSpace
+		// dest is users buffer
+
+		 os_memcpy((transferSpace + local_offset), buf_offset, (os_size_t) (BLOCKSIZE - local_offset)); 	// note, this updates the buf_offset pointer as it transfer the data
+		 																// os_memcpy takes uint32_t* as arguments
+		 fd->offset += (BLOCKSIZE - local_offset); // update the file descriptors file offset
+		 // reset transferSpace pointer
+		 transferSpace -= BLOCKSIZE;
+		 return (BLOCKSIZE - local_offset); // note, we are returning the number of bytes that were successfully transferred
+
+	} else if((local_offset > 0) && (bytesLeft < (BLOCKSIZE - local_offset)){
+	/*	______________________
+		|      |~~~~|         |
+		----------------------- */
+		// Actually move the data to the user's specified buffer...must first cast void pointers to uint32_t* pointers:
+		// source is transferSpace
+		// dest is users buffer
+
+		 os_memcpy((transferSpace + local_offset), buf_offset, (os_size_t) bytesLeft); 	// note, this updates the buf_offset pointer as it transfer the data
+		 																// os_memcpy takes uint32_t* as arguments
+		 fd->offset += bytesLeft; // update the file descriptors file offset
+		 // reset transferSpace pointer
+		 transferSpace -= (local_offset + bytesLeft);
+		 return bytesLeft; // note, we are returning the number of bytes that were successfully transferred
+
+	} else{
+		//this should never happen...print for debugging. TODO: remove after debugged
+		os_printf("Error! In f1() in kread()...this should never happend!");
+		return 0;
+	}//end if else block
+}//end of read_partial_block() helper function
+
+
+// Helper function for kread():
+int read_full_block(int bytesLeft, void* buf_offset, file_descriptor* fd, void* transferSpace) {
+	// read BLOCKSIZE
+	// Actually get the data for 1 block (the SD Driver will put it in transferSpace for us)
+	int blockNum = fd->offset / BLOCKSIZE;
+	int success = recieve(transferSpace, blockNum);
+	if(success < 0){
+	 	// failed on a block receive, therefore the whole kread fails; return failure error
+	 	os_printf("failed to receive block number %d\n", blockNum);
+	 	return -1;
+	}//end if
+	/*	______________________
+		|~~~~~~~~~~~~~~~~~~~~~|
+		----------------------- */
+	// Actually move the data to the user's specified buffer...must first cast void pointers to uint32_t* pointers:
+	// source is transferSpace
+	// dest is users buffer
+	 os_memcpy(transferSpace, buf_offset, (os_size_t) BLOCKSIZE); 	// note, this updates the buf_offset pointer as it transfer the data
+	 																// os_memcpy takes uint32_t* as arguments
+	 fd->offset += BLOCKSIZE; // update the file descriptors file offset
+	 // reset transferSpace pointer
+	 transferSpace -= BLOCKSIZE;
+	 return BLOCKSIZE; // note, we are returning the number of bytes that were successfully transferred
+}//end read_full_block() helper function
+
 
 /* read from fd, put it in buf, then return the number of bytes read in numBytes */
-int kread(int fd, void* buf, int numBytes) {
-	
-	/* Start of my idea of Algo */
-
+int kread(int fd_int, void* buf, int numBytes) {
 	int bytes_read = 0;
-	if(numBytes < BLOCKSIZE) {
-		while(bytes_read < numBytes) {
-			f1();
-		}
-	} else if(numBytes >= BLOCKSIZE) {
-		f1();
-	}
-	while((numBytes - bytes_read) > BLOCKSIZE) {
-		f2();
-	} 
-	if(bytes_read < numBytes) {
-		f1();
-	}
-
-
-	f1() {
-		int bytesLeftInBlock = offset % BLOCKSIZE;
-		int index = offset / BLOCKSIZE;
-		if(bytesLeftInBlock == 0) {
-			 ___________________
-			|~~~~~~~~~|			|
-			---------------------
-		} else if((((numBytes - bytes_read) + offset) / BLOCKSIZE) > index) {
-			_____________________
-			|           |~~~~~~~~|
-			----------------------
-		} else {
-			______________________
-			|      |~~~~|         |
-			-----------------------
-		}
-	}
-
-	f2() {
-		// read BLOCKSIZE
-	}
-
-	/* End my idea of Algo */
-
-
-
-	int bytes_read = 0;
-	void* buf_offset = buf; //this allows us to move data incrementally to user's buf via buf_offset
+	uint32_t* buf_offset = buf; //this allows us to move data incrementally to user's buf via buf_offset
 	//while retaining the original pointer to return back to the user
+	file_descriptor* fd = get_file_descriptor(fd_int); // note, get_file_descriptor() function has not yet been implemented...will be in open_table.c
 
-	struct file_descriptor filedescr = table[fd];
-	if (filedescr.permission != 'r' || filedescr.permission != 'b') {
+	if (fd->permission != 'r' || fd->permission != 'b') {
 		os_printf("no permission \n");
 		return -1;
 	}
 
 	// Allocate space for and create a bitvector to be used repeatedly to transfer the data:
-	void* transferSpace = kmalloc(BLOCKSIZE);
-	int ct = 0; //for debugging only TODO: remove once debugged
+	uint32_t* transferSpace = kmalloc(BLOCKSIZE);
 	int blockNum = 0;
-	if(numBytes < BLOCKSIZE){
 
-
-
-
-
-
-
-
-	}else if(numBytes > BLOCKSIZE){
-		//read and transfer first chunk:
-		blockNum = fd->offset / BLOCKSIZE;
-	 	int success = recieve(transferSpace, blockNum);
-	 	if(success < 0){
-	 		// failed on a block receive, therefore the whole kread fails; return failure error
-	 		os_printf("failed to receive block number %d\n", ct);
-	 		return -1;
-	 	}//end if
-	 	ct++;
-		// dif is the difference between the current offset and the end of the bloc (always less than BLOCKSIZE)
-		int dif = fd->offset % BLOCKSIZE;
-		// Actually move the data:
-		int i;
-		for(i = 0; i < dif; i++){
-			*buf_offset = *(transferSpace + (BLOCKSIZE - dif) +)
-			buf_offset ++;
-			bytes_read ++;
-			fd->offset ++;
-		}//end for
-		//DONE READING FIRST CHUNK, now fd-> is pointing to start of block
-		
-
-
+	// start of higher-level algo:
+	int bytes_read = 0;
+	if(numBytes < BLOCKSIZE) {
+		while(bytes_read < numBytes) {
+			bytes_read += read_partial_block((numBytes-bytes_read), buf_offset, fd, transferSpace);
+		}
+	} else if(numBytes >= BLOCKSIZE) {
+		bytes_read += read_partial_block((numBytes-bytes_read), buf_offset, fd, transferSpace);
+		while((numBytes - bytes_read) > BLOCKSIZE) {
+			bytes_read += read_full_block((numBytes-bytes_read), buf_offset, fd, transferSpace);
+		}
+		if(bytes_read < numBytes) {
+			bytes_read += read_partial_block((numBytes-bytes_read), buf_offset, fd, transferSpace);
+		}
+	}//end else if
+	if(bytes_read != numBytes){
+		return bytes_read;
 	}else{
-
-
-
-
-
-
-	}
-
-
-
-
-	while(numBytes > BLOCKSIZE){
-		blockNum = fd->offset / BLOCKSIZE;
-	 	int success = recieve(transferSpace, blockNum);
-	 	if(success < 0){
-	 		// failed on a block receive, therefore the whole kread fails; return failure error
-	 		os_printf("failed to receive block number %d\n", ct);
-	 		return -1;
-	 	}//end if
-	 	ct++;
-		numBytes -= BLOCKSIZE;
-		if(fd->)
-	}//end while
-
-	if(numBytes > 0){
-		// then there is more to be read, but less than 1 block
-		int success = recieve(transferSpace, );
-	 	if(success < 0){
-	 		// failed on a block receive, therefore the whole kread fails; return failure error
-	 		os_printf("failed to receive block number %d\n", ct);
-	 		return -1;
-	 	}//end if
-	 	ct++;
-	}
-
-	recieve();
-	//GET RETURN BUFFER FROM RECIVE() 
-
-	return bytes_read;
+		return -1;
+	}	
 } // end kread();
 
 
@@ -238,14 +227,14 @@ int kclose(int fd) {
 
 
 /* seek within the file, return an error if you are outside the boundaries */
-int kseek(int fd, int num_bytes) {
+int kseek(int fd_int, int num_bytes) {
 	int error;
-        struct file_descriptor filedescr = table[fd];
-        if (filedescr.permission != 'r' || filedescr.permission != 'w' || filedescr.permission != 'b') {
+        file_descriptor* fd = get_file_descriptor(fd_int);
+        if (fd->permission != 'r' || fd->permission != 'w' || fd->permission != 'b') {
                 os_printf("no permission \n");
                 return -1;
         }
-	filedescr.offset += num_bytes;	
+	fd->offset += num_bytes;	
 	return 0;
 } // end kseek();
 
