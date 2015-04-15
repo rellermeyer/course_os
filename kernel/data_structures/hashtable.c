e70b980dd1ea74923feba9045b437d2d05e9cf19
Lane Kolbly <lane@rscheme.org>
2015-04-14 20:36:32 -0500

    Got hash tables working

--- kernel/data_structures/hashtable.c
+++ kernel/data_structures/hashtable.c
@@ -1,8 +1,14 @@
 #include "klibc.h"
 #include "hashtable.h"
 
+// "str" must be null-terminated
 uint32_t hash_string(const char *str) {
-	return 0;
+	uint32_t hash = 123456789;
+	int i;
+	for (i=0; str[i]; i++) {
+		hash = hash*33 + str[i];
+	}
+	return hash;
 }
 
 struct ht *ht_alloc(int nentries) {
@@ -23,6 +29,7 @@ struct ht_entry *ht_get_entry(struct ht *table, const char *key)
 }
 
 int ht_add(struct ht *table, const char *key, void *value) {
+	os_printf("Adding %s...\n", key);
 	uint32_t hash = hash_string(key);
 	struct ht_entry *entry = &table->entries[hash%table->size];
 	while (1) {
@@ -39,13 +46,38 @@ int ht_add(struct ht *table, const char *key, void *value) {
 			// Fill this entry
 			break;
 		}
+		entry = entry->next_entry;
 	}
 	entry->value = value;
+	entry->key.hash = hash;
+	entry->key.key = kmalloc(os_strlen(key)+1);
+	os_strcpy(entry->key.key, key);
 	return 0;
 }
 
 int ht_remove(struct ht *table, const char *key) {
-	// Currently unimplemented
+	uint32_t hash = hash_string(key);
+	struct ht_entry *entry = &table->entries[hash%table->size];
+	struct ht_entry *last = 0x0;
+	while (1) {
+		if ((!entry) || !entry->key.key) {
+			return 0;
+		}
+		if (os_strcmp(entry->key.key, key)) {
+			entry = entry->next_entry;
+		} else {
+			// We found it.
+			kfree(entry->key.key);
+			entry->key.key = 0x0;
+			entry->value = 0x0;
+			if (last) {
+				last->next_entry = entry->next_entry;
+			} else if (entry->next_entry) {
+				os_memcpy((uint32_t*)entry, (uint32_t*)entry->next_entry, sizeof(struct ht_entry));
+			}
+			return 1;
+		}
+	}
 	return -1;
 }
 
@@ -55,4 +87,51 @@ void *ht_get(struct ht *table, const char *key) {
 		return 0x0;
 	}
 	return entry->value;
+}
+
+void ht_test_hasher(const char *key) {
+	os_printf("'%s' hashes to %u\n", key, hash_string(key));
+}
+
+void ht_test() {
+	os_printf("***** Test code for hash table (ht_test()): *****\n");
+
+	// Let's test the string hashing function for a few strings
+	ht_test_hasher("");
+	ht_test_hasher("Lane");
+	ht_test_hasher("Mark");
+	ht_test_hasher("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNRSTUVWXYZ\r\n");
+
+	// Now test the actual table...
+	struct ht *table = ht_alloc(6);
+
+	// Add a bunch of stuff (load factor = 1.5, chaining is tested)
+	ht_add(table, "abcde", (void*)1);
+	ht_add(table, "ABCDE", (void*)2);
+	ht_add(table, "EDACSDAFG", (void*)3);
+	ht_add(table, "aaaaaaaaaa", (void*)4);
+	ht_add(table, "abcdej", (void*)5);
+	ht_add(table, "abcdei", (void*)6);
+	ht_add(table, "abcdeh", (void*)7);
+	ht_add(table, "abcdeg", (void*)8);
+	ht_add(table, "abcdef", (void*)9);
+
+	// Retrieve some stuff...
+	if ((int)ht_get(table, "abcdeh") == 7) {
+		os_printf("1 :)\n");
+	} else {
+		os_printf("1 :(\n");
+	}
+
+	// Remove some stuff...
+	ht_remove(table, "abcdeg");
+
+	// Make sure it's gone...
+	if ((int)ht_get(table, "abcdeg") == 0) {
+		os_printf("2 :)\n");
+	} else {
+		os_printf("2 :(\n");
+	}
+
+	os_printf("*************************************************\n");
 }
