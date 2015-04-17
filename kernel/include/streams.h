90a3c03116957f19407e8bbd8074b7f54e1e5b26
mark <mark.r.daniel@gmail.com>
2015-04-16 20:40:19 -0500

    added array and corrected queue descriptor

--- kernel/include/streams.h
+++ kernel/include/streams.h
@@ -2,25 +2,35 @@
 #define __streams_h
 #include <stdint.h>
 
-struct stream
-{
-	int_least32_t *data;
-	int_least32_t length;
-	int_least32_t offset;
-};
+//struct stream
+//{
+//	int_least32_t *data;
+//	int_least32_t length;
+//	int_least32_t offset;
+//};
 
 struct queue
 {
 	int_least32_t *data;
 	int_least32_t datalen;
 	char *q_name;
+    struct queue *qd;
 	char *options;
-	struct queue *next;
+	struct subscriber *subscriber;
+    //struct subscriber *subscriber_head;//to implement if change to multi- subscriber model
+    //struct subscriber *subscriber_tail;
+    struct queue *next;
+};
+
+struct subscriber
+{
+    int_least32_t *userdata;
+//    int_least32_t *next;
 };
 
 void q_create(char q_name[], char options[]);
-struct queue *q_open(char q_name[]);
-int_least32_t q_publish(struct queue *q, int_least32_t *data, int_least32_t datalen);
+int_least32_t q_open(char q_name[]);
+int_least32_t q_publish(int_least32_t qd, int_least32_t *data, int_least32_t datalen);
 int_least32_t receiver(int_least32_t *userdata, int_least32_t *data, int_least32_t datalen);
 int_least32_t q_subscribe(int_least32_t q, void (*receiver)(int_least32_t *userdata, int_least32_t *data, int_least32_t datalength), int_least32_t *userdata);
 void q_test();
