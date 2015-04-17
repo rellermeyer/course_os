3ce2f55b5764d8a475c2493e587ea66ed11583c3
c4k9c0 <charliecox17@yahoo.com>
2015-04-16 19:54:12 -0500

    Changed all assembly to the correct syntax

--- kernel/hw_handlers.c
+++ kernel/hw_handlers.c
@@ -90,11 +90,11 @@ void  __attribute__((interrupt("SWI"))) software_interrupt_handler(void){
 		os_printf("Delete system call called!\n");
 		char* filepath;
 		// retrieve the args that delete() put in r1 and pass to kdelete():
-		asm volatile("mov %0, r1": "(filepath)");
+		asm volatile("mov r1, %[filepath]");
 		// call kdelete(), passing appropriate args:
 		int error = kdelete(fd);
 		// move error that kdelete() returns to a r1 to be retrieved by delete() and returned to user:
-		asm volatile("mov %0, (error)" : "r1");
+		asm volatile("mov %[error], r1");
 		break;
 
 	case SYSCALL_OPEN:
@@ -102,12 +102,12 @@ void  __attribute__((interrupt("SWI"))) software_interrupt_handler(void){
 		char* filepath;
 		char mode;
 		// retrieve the args that open() put in r1, r2 and pass to kopen():
-		asm volatile("mov %0, r1": "(filepath)");
-		asm volatile("mov %0, r2": "(mode)");
+		asm volatile("mov r1, %[filepath]");
+		asm volatile("mov r2, %[mode]");
 		// call kopen(), passing appropriate args:
 		int fd = kopen(filepath, mode);
 		// move fd that kopen() returns to a r1 to be retrieved by open() and returned to user:
-		asm volatile("mov %0, (fd)" : "r1");
+		asm volatile("mov %0[fd], r1");
 		break;
 
 	case SYSCALL_CREATE:
@@ -115,12 +115,12 @@ void  __attribute__((interrupt("SWI"))) software_interrupt_handler(void){
 		char* filepath;
 		char mode;
 		// retrieve the args that open() put in r1, r2 and pass to kopen():
-		asm volatile("mov %0, r1": "(filepath)");
-		asm volatile("mov %0, r2": "(mode)");
+		asm volatile("mov r1, %[filepath]");
+		asm volatile("mov r2, %[mode]");
 		// call kopen(), passing appropriate args:
 		int error = kopen(filepath, mode);
 		// move fd that kopen() returns to a r1 to be retrieved by open() and returned to user:
-		asm volatile("mov %0, (fd)" : "r1");
+		asm volatile("mov %[fd], r1");
 		break;
 
 	case SYSCALL_READ:
@@ -129,14 +129,14 @@ void  __attribute__((interrupt("SWI"))) software_interrupt_handler(void){
 		void* buf;
 		int numBytes;
 		// retrieve the args that read() put in r1, r2 and pass to kread():
-		asm volatile("mov %r1, %[fd]");
-		asm volatile("mov %0, r2": "(buf)");
-		asm volatile("mov %0, r3": "(numBytes)");
+		asm volatile("mov r1, %[fd]");
+		asm volatile("mov r2, %[buf]");
+		asm volatile("mov r3, %[numBytes]");
 		// call kread(), passing appropriate args:
 		// TODO: macro to translate process's virtual memory into kernel's view of virtual memory
 		int bytesRead = kread(fd, buf, numBytes);
 		// move fd that kread() returns to a r1 to be retrieved by read() and returned to user:
-		asm volatile("mov %0, (bytesRead)" : "r1");
+		asm volatile("mov %[bytesRead], r1");
 		break;
 	case SYSCALL_WRITE:
 		os_printf("Write system call called!\n");
@@ -144,35 +144,35 @@ void  __attribute__((interrupt("SWI"))) software_interrupt_handler(void){
 		void* buf;
 		int numBytes;
 		// retrieve the args that write() put in r1, r2 and pass to kwrite():
-		asm volatile("mov %0, r1": "(fd)");
-		asm volatile("mov %0, r2": "(buf)");
-		asm volatile("mov %0, r3": "(numBytes)");
+		asm volatile("mov r1, %[fd]");
+		asm volatile("mov r2, %[buf]");
+		asm volatile("mov r3, %[numBytes]");
 		// call kwrite(), passing appropriate args:
 		int bytesWritten = kwrite(fd, buf, numBytes);
 		// move fd that kwrite() returns to a r1 to be retrieved by write() and returned to user:
-		asm volatile("mov %0, (bytesWritten)" : "r1");
+		asm volatile("mov %[bytesWritten], r1");
 		break;
 	case SYSCALL_CLOSE:
 		os_printf("Close system call called!\n");
 		int fd;
 		// retrieve the args that close() put in r1 and pass to kclose():
-		asm volatile("mov %0, r1": "(fd)");
+		asm volatile("mov r1, %[fd]");
 		// call kclose(), passing appropriate args:
 		int error = kclose(fd);
 		// move error that kclose() returns to a r1 to be retrieved by close() and returned to user:
-		asm volatile("mov %0, (error)" : "r1");
+		asm volatile("mov %[error], r1");
 		break;
 	case SYSCALL_SEEK:
 		os_printf("Seek system call called!\n");
 		int fd;
 		int numBytes;
 		// retrieve the args that seek() put in r1, r2 and pass to kseek():
-		asm volatile("mov %0, r1": "(fd)");
-		asm volatile("mov %0, r2": "(numBytes)");
+		asm volatile("mov r1, %[fd]");
+		asm volatile("mov r2, %[numBytes]");
 		// call kseek(), passing appropriate args:
 		int error = kseek(fd, numBytes);
 		// move error that kseek() returns to a r1 to be retrieved by seek() and returned to user:
-		asm volatile("mov %0, (error)" : "r1");
+		asm volatile("mov %[error], r1");
 		break;
 	case SYSCALL_SET_PERM:
 		os_printf("Set permission system call called!\n");
@@ -195,6 +195,7 @@ void __attribute__((interrupt("ABORT"))) prefetch_abort_handler(void){
 void __attribute__((interrupt("ABORT"))) data_abort_handler(void){
 	os_printf("DATA ABORT HANDLER\n");
 	int pc, lr, sp, fp;
+	// not sure this is correct syntax, did we [Spring 2015 do this?]
 	asm volatile("mov %0, pc" : "=r" (pc));
 	asm volatile("mov %0, lr" : "=r" (lr));
 	asm volatile("mov %0, sp" : "=r" (sp));
