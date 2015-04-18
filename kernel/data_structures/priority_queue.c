bd92d4332a601552ad0590c420801cc2be323be9
Mathew Kurian <bluejamesbond@gmail.com>
2015-04-17 22:51:55 -0700

    Working on scheduler/ build broken

--- kernel/data_structures/priority_queue.c
+++ kernel/data_structures/priority_queue.c
@@ -1,78 +1,107 @@
 #include "../include/priority_queue.h"
 #include "klibc.h"
 
-void __prq_insert(prq_node node, prq_node* heap, int size);
-void __prq_shift_down(prq_node* heap, int size, int idx);
-prq_node __prq_remove_min(prq_node * heap, int size);
-prq_node __prq_remove_min(prq_node * heap, int size);
+void __prq_shift_up(prq_handle * queue, int idx);
+void __prq_shift_down(prq_handle * queue, int idx);
 
-void __prq_insert(prq_node node, prq_node* heap, int size) {
-	int idx;
-	prq_node tmp;
-	idx = size + 1;
-	heap[idx] = node;
-	while (heap[idx].priority < heap[idx / 2].priority && idx > 1) {
-		tmp = heap[idx];
-		heap[idx] = heap[idx / 2];
-		heap[idx / 2] = tmp;
-		idx /= 2;
-	}
+void __prq_shift_up(prq_handle * queue, int idx) {
+    prq_node ** heap = queue->heap;
+    prq_node * node, *parent;
+    while (heap[idx]->priority < heap[idx / 2]->priority && idx > 1) {
+        node = heap[idx];
+        parent = heap[idx / 2];
+        parent->index = idx;
+        node->index = idx / 2;
+        heap[idx] = parent;
+        heap[idx / 2] = node;
+        idx /= 2;
+    }
 }
 
-void __prq_shift_down(prq_node* heap, int size, int idx) {
-
-	int cidx;        //index for child
-	prq_node tmp;
-	for (;;) {
-		cidx = idx * 2;
-		if (cidx > size) {
-			break;   //it has no child
-		}
-		if (cidx < size) {
-			if (heap[cidx].priority > heap[cidx + 1].priority) {
-				++cidx;
-			}
-		}
-
-		//swap if necessary
-		if (heap[cidx].priority < heap[idx].priority) {
-			tmp = heap[cidx];
-			heap[cidx] = heap[idx];
-			heap[idx] = tmp;
-			idx = cidx;
-		} else {
-			break;
-		}
-	}
+void __prq_shift_down(prq_handle * queue, int idx) {
+    int cidx;        //index for child
+    prq_node ** heap = queue->heap;
+    prq_node * node, *child;
+    for (;;) {
+        cidx = idx * 2;
+        if (cidx > size) {
+            break;   //it has no child
+        }
+        if (cidx < size) {
+            if (heap[cidx]->priority > heap[cidx + 1]->priority) {
+                ++cidx;
+            }
+        }
+        //swap if necessary
+        if (heap[cidx]->priority < heap[idx]->priority) {
+            node = heap[cidx];
+            child = heap[idx];
+            node->index = idx;
+            child->index = cidx;
+            heap[cidx] = child;
+            heap[idx] = node;
+            idx = cidx;
+        } else {
+            break;
+        }
+    }
 }
 
-prq_node __prq_remove_min(prq_node * heap, int size) {
-	prq_node rv = heap[1];
-	if (PRQ_DEBUG) {
-		os_printf("%d:%d:%d\n", size, heap[1].priority, heap[size].priority);
-	}
-	heap[1] = heap[size];
-	--size;
-	__prq_shift_down(heap, size, 1);
-	return rv;
+prq_node * prq_peek(prq_handle * queue) {
+    if (queue) {
+        if (queue->size > 0) {
+            return queue->heap[1];
+        }
+    }
 
+    return 0;
 }
 
-void prq_enqueue(prq_node node, prq_handle * queue) {
-	__prq_insert(node, queue->heap, queue->size);
-	++queue->size;
+int prq_size(prq_handle * queue) {
+    if (queue) {
+        return queue->size;
+    }
+
+    return 0;
 }
 
-prq_node prq_dequeue(prq_handle * queue) {
-	prq_node rv = __prq_remove_min(queue->heap, queue->size);
-	--queue->size;
-	return rv;
+void prq_enqueue(prq_handle * queue, prq_node * node) {
+    prq_node ** heap = queue->heap;
+    int index = node->index = size + 1;
+    heap[index] = node;
+    ++queue->size;
+    __prq_shift_up(heap, index);
+}
+
+void prq_dequeue(prq_handle * queue, prq_node * node) {
+    prq_node ** heap = queue->heap;
+    prq_node * end = heap[size];
+    int index = (end->index = node->index);
+    node->index = -1;
+    heap[index] = end;
+    heap[size] = 0;
+    --queue->size;
+    // FIXME optimize later
+    __prq_shift_up(queue, index);
+    __prq_shift_down(queue, index);
+}
+
+void prq_dequeue(prq_handle * queue) {
+    prq_node ** heap = queue->heap;
+    prq_node * top = heap[1];
+    prq_node * end = heap[size];
+    end->index = 1;
+    top->index = -1;
+    heap[1] = end;
+    heap[size] = 0;
+    --queue->size;
+    __prq_shift_down(queue, 1);
 }
 
 // FIXME @CalvinBench
 // Add support for variable length priority queues
 // Add test cases as necessary
 void prq_init(prq_handle * queue, int n) {
-	queue->size = 0;
-	queue->heap = (prq_node*) kmalloc(sizeof(prq_node) * (n + 1));
+    queue->size = 0;
+    queue->heap = (prq_node**) kmalloc(sizeof(prq_node*) * (n + 1));
 }
--- kernel/process/scheduler.c
+++ kernel/process/scheduler.c
@@ -1,7 +1,156 @@
-#include "../include/mem_alloc.h"
 #include "../include/scheduler.h"
-#include "klibc.h"
 
-void init_sched(){
-	os_printf("Initializing scheduler\n");
+#define MAX_TASKS 100
+#define MAX_ACTIVE_TASKS 4
+#define MAX_NICENESS -20
+#define MIN_NICENESS 20
+#define TASK_STATE_FINSIHED 3
+#define TASK_STATE_INACTIVE  2
+#define TASK_STATE_ACTIVE 1
+#define TASK_STATE_NONE 0
+#define MAX(a, b) 	a > b ? a : b
+#define MIN(a, b)   a < b ? a : b
+
+static char * last_err;
+static prq_handle all_tasks;
+static prq_handle active_tasks;
+static int ignore_interrupt;
+static sched_task * active_task;
+
+void sched_init() {
+    os_printf("Initializing scheduler\n");
+    last_err = "No error";
+    init_queue(&all_tasks, MAX_TASKS);
+    init_queue(&active_tasks, MAX_ACTIVE_TASKS);
+    active_task = 0;
+}
+
+sched_task* sched_create_task(uint32_t* file_p, int niceness) {
+    if (prq_size(&all_tasks) >= MAX_TASKS) {
+        last_err = "Too many tasks";
+        return 0;
+    }
+
+    ignore_interrupt = 1;
+
+    // use the kernel memory
+    if(active_task != 0){
+        vm_use_kernel_vas();
+    }
+
+    pcb * pcb_pointer = process_create(file_p);
+
+    if (!pcb_pointer) {
+        last_err = "Process create error";
+        ignore_interrupt = 0;
+        return 0;
+    }
+
+    struct vas * vas_struct = vm_new_vas();
+    sched_task * task = (sched_task*) kmalloc(sizeof(sched_task));
+
+    // FIXME
+    // Check this code @lkolbly
+    // set up the vas
+    // call vm_set_mapping(struct vas *vas, void *vptr, void *pptr, int permission);
+    // not sure how to use it
+
+    task->niceness = MAX(MAX_NICENESS, MIN(MIN_NICENESS, niceness));
+    task->pcb = pcb_pointer;
+    task->state = TASK_STATE_NONE;
+    task->node = 0;
+    task->vas_struct = vas_struct;
+
+    if(active_task != 0){
+        vm_enable_vas(active_task->vas_struct);
+    }
+
+    ignore_interrupt = 0;
+
+    return task;
+
+}
+
+void sched_interrupt_handler() {
+    if (ignore_interrupt) {
+        return;
+    }
+
+    ignore_interrupt = 1; // prevent interrupts while handling another interrupt
+
+    if (prq_size(&active_tasks) < MAX_ACTIVE_TASKS) {
+        if (prq_size(&all_tasks) > 0) {
+            prq_node * node = prq_dequeue(&all_tasks);
+            sched_task * task = (sched*) node->data;
+            prq_enqueue(&active_tasks, task, task->niceness); // add to active_tasks if the task
+        }
+    }
+
+    if (prq_size(&active_tasks) == 0) {
+        return;
+    }
+
+    sched_task * task;
+
+    // check if there is active task
+    if (active_task == 0) {
+        task = active_task;
+    } else {
+        prq_node node = prq_peek(&active_tasks);
+        task = (sched*) node->data;
+    }
+
+    switch (task->state) {
+        case TASK_STATE_NONE:
+        case TASK_STATE_INACTIVE: 				      // start running task->pcb
+            pcb * pcb_pointer = task->pcb;
+            task->state = TASK_STATE_ACTIVE;	  // mark task process as active
+            active_task = task;					  // set the active_task pointer
+            ignore_interrupt = 0;				        // allow interrupts
+            execute_process(&task->pcb);   // start running the process function
+            ignore_interrupt = 1;				        // disallow interrupts
+            task->state = TASK_STATE_FINSIHED;	        // process completed
+            free_pcb(&task->pcb);						// free process memory
+            prq_dequeue(&active_tasks, *task->node); // remove it from the queue
+
+            // NOTE next interrupt will get the start the process
+
+            break;
+        case TASK_STATE_ACTIVE: 							// sleep task->pcb
+            if (prq_size(&active_tasks) > 1) { // check if there at least one another task in the queue
+                prq_node node = prq_dequeue(&active_tasks, active_task->node); // remove the running task from queue
+                prq_enqueue(&active_tasks, node);
+                pcb * pcb_pointer = task->pcb;
+                save_process_state(pcb_pointer->PID); // save the current state of the process
+                load_process_state(pcb_pointer->PID);
+            }
+
+            break;
+    }
+
+    ignore_interrupt = 0;
+}
+
+uint32_t sched_add_task(sched_task * task) {
+    if (task) {
+        if (task->state == TASK_STATE_NONE) {
+            last_err = "Reusing task object not allowed";
+            return 0;
+        }
+
+        task->node = prq_enqueue(&all_tasks, task, task->niceness);
+        return 1;
+    }
+
+    last_err = "Invalid sched_task pointer";
+    return 0;
+}
+
+uint32_t sched_remove_task(sched_task * task) {
+    return 1;
+}
+
+const char *
+sched_last_err() {
+    return last_err;
 }
