8f2b5848ac1a68914ba3aa2d3abb26a3a3961ff1
c4k9c0 <charliecox17@yahoo.com>
2015-04-18 17:57:48 -0500

    Finished syntax with fs_syscall.c, getting cross compile error. Asking Lane tomorrow

--- user/lib/fs_syscalls.c
+++ user/lib/fs_syscalls.c
@@ -149,12 +149,13 @@ int close(int fd){
 
 /*seek() returns the number of bytes that the file offset is successfully moved
   if successful, and -1 if the numBytes puts puts them outside the file length*/
-int seek(int fd, int numBytes){
+int seek(int fd_int, int numBytes){
 	int error; //-1 if numBytes + offset exceeds file length. This will be return.
 	// Error checking...no need to go into kernel if numBytes is invalid:
+	struct file_descriptor* fd = get_descriptor(fd_int);
 	if(numBytes == 0){
 		return 0;
-	} else if ((numBytes > 0) && ((fd->offset + numBytes) > (fd->file)->size)){
+	} else if ((numBytes > 0) && ((fd->offset + numBytes) > ((fd->linked_file)->inode)->size)){
 		os_printf("Error! file offset exceeds file size \n");
 		return -1;
 	}else if ((numBytes < 0) && ((fd->offset + numBytes) < 0)){
@@ -163,15 +164,15 @@ int seek(int fd, int numBytes){
 	}//end if else
 
 	// move the arguments into registers for kseek() to use:
-	asm volatile("mov %[fd], r1":: "r1");
-	asm volatile("mov %[numBytes], r2":: "r2");
+	asm volatile("mov %[fd1], r1"::[fd1]"r" (fd): "r1");
+	asm volatile("mov %[numBytes1], r2"::[numBytes1]"r" (fd): "r2");
 
 	// trigger the software_interrupt_handler in hw_handler.c:
 	asm volatile("swi SYSCALL_SEEK");
 	// kseek() gets called from within hw_handler.c	
 
 	// retrieve the return values from kseek to pass back to user:
-	asm volatile("mov r1, %[error]");
+	asm volatile("mov r1, %[error1]":[error1]"=r" (error)::);
 
 	return error;
 }//end seek syscall
@@ -185,14 +186,14 @@ int delete(char* filepath){
 	// probably will have to check  if file exists in kdelete, but check here if possible...
 
 	// move the arguments into registers for kdelete() to use:
-	asm volatile("mov %[filepath], r1":: "r1");
+	asm volatile("mov %[filepath1], r1"::[filepath1]"r" (filepath): "r1");
 
 	// trigger the software_interrupt_handler in hw_handler.c:
 	asm volatile("swi SYSCALL_DELETE");
 	// kdelete() gets called from within hw_handler.c
 
 	// retrieve the return values from kdelete to pass back to user:
-	asm volatile("mov r1, %[error]");
+	asm volatile("mov r1, %[error1]":[error1]"=r" (error)::);
 
 	return error;
 }//end delete syscall
