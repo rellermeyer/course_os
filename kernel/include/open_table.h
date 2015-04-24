ce8aff7171c35c0c3d0cd910f21f6d6e13572fb5
c4k9c0 <charliecox17@yahoo.com>
2015-04-24 13:29:16 -0500

    More change, syntax fix

--- kernel/include/open_table.h
+++ kernel/include/open_table.h
@@ -27,13 +27,13 @@ struct free_index {
 void fs_table_init(); //initializes fs open table at boot up time
 void fs_table_shutdown(); //closes the open table at shutdown time
 
-int add_to_opentable(struct inode * f, char perm);  //adds a file to the opentable, returns fd if successful and -1 if not
+int add_to_opentable(struct inode*, char);  //adds a file to the opentable, returns fd if successful and -1 if not
 
-int delete_from_opentable(int fd); //deletes a file from the opentable, returns 0 if successful, -1 if not
+int delete_from_opentable(int); //deletes a file from the opentable, returns 0 if successful, -1 if not
 
-int file_is_open(int fd; //checks if there is an entry corrensponding to that fd
+int file_is_open(int); //checks if there is an entry corrensponding to that fd
 
-struct file_descriptor* get_descriptor(int fd); //returns the filedescriptor struct linked to fd 
+struct file_descriptor* get_descriptor(int); //returns the filedescriptor struct linked to fd 
 
 #include "file.h"
 
