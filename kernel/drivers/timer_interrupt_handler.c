a0c52b7083f69f9b19ab05b944bf63e2dbbd4b63
Pradyumn Pagadala <pradyumn@utexas.edu>
2015-04-25 15:41:23 -0500

    fixes to handler

--- kernel/drivers/timer_interrupt_handler.c
+++ kernel/drivers/timer_interrupt_handler.c
@@ -1,13 +1,13 @@
-#include "include/interrupt.h"
-#include "klibc.h"
-#include "include/timer.h"
+#include "../include/interrupt.h"
+#include "../include/klibc.h"
+#include "../include/timer.h"
 
 
 // Create the handler
 void _schedule_register_timer_irq(){
 	interrrupt_handler_t timer;
 	timer->handler=&timer_interrupt_handler;
-	register_interrupt_handler(4, timer)
+	register_interrupt_handler(4, timer);
 	timer_start();
 }
 
