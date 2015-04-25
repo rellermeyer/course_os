707d5c05b5c2b105ce6dd721e8d73ba44ba7b47f
Ginevra <ginevra.gaudioso@utexas.edu>
2015-04-25 15:17:02 -0500

    added comments to bitvector header -Ginevra

--- kernel/include/bitvector.h
+++ kernel/include/bitvector.h
@@ -9,12 +9,12 @@ typedef struct bitVector {
 	uint32_t actualLength;
 } bitVector;
 
-bitVector *makeVector(uint32_t);
-int32_t bv_get(uint32_t, bitVector*);
-int32_t bv_toggle(uint32_t, bitVector*);
-int32_t bv_set(uint32_t, bitVector*);
-int32_t bv_lower(uint32_t, bitVector*);
-int32_t bv_firstFree(bitVector*);
-int32_t bv_free(bitVector*);
+bitVector *makeVector(uint32_t size); //creates butvector of "size" number of bits
+int32_t bv_get(uint32_t index, bitVector* bitVec); //gets the value of bit number "index"
+int32_t bv_toggle(uint32_t index, bitVector* bitVec); //flips the bit number "index"
+int32_t bv_set(uint32_t index, bitVector* bitVec); //sets the value of bit number "index" to 1 (meaning taken)
+int32_t bv_lower(uint32_t index, bitVector* bitVec); //sets the value of bit number "index" to 0 (meaning free)
+int32_t bv_firstFree(bitVector* bitVec); //returns the first free index (first 0 in the vector)
+int32_t bv_free(bitVector* bitVec); //frees the memory used by the vector
 
 #endif 
