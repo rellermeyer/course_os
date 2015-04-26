9db4dee0d3671ec8be56449c5329eb3d9b21db7a
Jorge Munoz <jorgmuno@cs.utexas.edu>
2015-04-26 15:33:38 -0500

    still testing

--- kernel/fun.c
+++ kernel/fun.c
@@ -3,7 +3,7 @@ typedef struct interrupt_handler_t
 {
 	void (*handler)(void *args);
 } interrupt_handler_t; 
-//static interrupt_handler_t *handlers[12];
+static interrupt_handler_t *handlers[12];
 int register_interrupt_handler(int num, interrupt_handler_t *handler){
 	handlers[num] = handler;
 	return 0;
@@ -15,13 +15,13 @@ printf("hello I'm interrupting");
 
 void _schedule_register_timer_irq(){
         interrupt_handler_t *timer;
-       // timer->handler=&timer_interrupt_handler;
-      // register_interrupt_handler(4,timer);
+        timer->handler=&timer_interrupt_handler;
+       register_interrupt_handler(4,timer);
        // initialize_timers();
 }
 void main(){
-	int *x;
-	*x=5;
+	int x;
+	x=5;
 	_schedule_register_timer_irq();
-	//handlers[4]->handler((void *) x);
+	handlers[4]->handler((void *)&x);
 }
