84ba96a6159fd6f162f0e15cbed9363bf19b5761
Lane Kolbly <lane@rscheme.org>
2015-05-01 12:08:43 -0500

    Got FS command line to add files from directory

--- kernel/fs/cmdline/bitvector.h
+++ kernel/fs/cmdline/bitvector.h
@@ -1,12 +1,39 @@
-#ifndef BITVECTOR_H
-#define BITVECTOR_H
+#ifndef BITVECTOR_H_
+#define BITVECTOR_H_
 
-#include<stdint.h>
+#include <stdint.h>
 
 typedef struct bit_vector {
-        uint32_t length;
-        uint32_t *vector;
-        uint32_t actualLength;
+	uint32_t length;
+	uint32_t *vector;
+	uint32_t actualLength;
 } bit_vector;
 
-#endif
+/* creates butvector of "size" number of bits */
+bit_vector *make_vector(uint32_t size);
+
+/* gets the value of bit number "index" */
+int32_t bv_get(uint32_t index, bit_vector* bit_vec);
+
+/* flips the bit number "index" */
+int32_t bv_toggle(uint32_t index, bit_vector* bit_vec);
+
+/* sets the value of bit number "index" to 1 (meaning taken) */
+int32_t bv_set(uint32_t index, bit_vector* bit_vec);
+
+/* sets the value of bit number "index" to 0 (meaning free) */
+int32_t bv_lower(uint32_t index, bit_vector* bit_vec);
+
+/* returns the first free index (first 0 in the vector) */
+int32_t bv_firstFree(bit_vector* bit_vec);
+
+/* frees the memory used by the vector */
+int32_t bv_free(bit_vector* bit_vec);
+
+/* transfer a bit_vector to disk */
+int32_t bv_serialize(bit_vector* bit_vec, uint32_t start_block, uint32_t end_block);
+
+/* transfer a bit_vector from disk */
+int32_t bv_unserialize(bit_vector* bit_vec, uint32_t start_block, uint32_t end_block);
+
+#endif 
