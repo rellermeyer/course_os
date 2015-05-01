61b515fd69c19bf683696dc10d2a557c714ff6e8
Jesse Thaden <noel.blob@gmail.com>
2015-05-01 03:07:14 -0500

    Finished all relevant functions for swap functions to SD (save page_fault) and updated gitignore with Jesse Thaden

--- kernel/vm/swap_framework.h
+++ kernel/vm/swap_framework.h
@@ -6,6 +6,7 @@
 #include "klibc.h"
 #include <stdint.h>
 #include "swap_pqueue.h"
+#include "swap_fs.h"
 
 // Contributors: Noel Negusse and Jesse Thaden
 /* Function: swap_framework
@@ -22,7 +23,7 @@
  */
 
 #define SWAP_SPACES (1<<8)
-#define PAGE_ENTRIES (11<<12) // Assuming 4kB Pages right now
+#define PAGE_ENTRIES (1<<12) // Assuming 4kB Pages right now
 #define PAGE_SIZE (1<<12) // May carry an if statement later...
 
 struct swap_space {
@@ -40,8 +41,8 @@ struct swap_entry {
 	struct swap_entry *next;
 	uint32_t higher_bits; // swap entry ID [24-bit assuming 4kB pages]
 	uint16_t e_flags; // ENT_USED (1000 or 1), ENT_WRITEOK (0100 or 2) OR BOTH (1100 or 3)
-        uint8_t free;
-        void *data;
+        uint8_t free; //0 - used, 1 - free
+        void *page; // virtual address pointer used for resolving page faults
 }; // Total: 15 bytes 
 
 struct swap_space *front;
@@ -61,19 +62,17 @@ void swap_init();
  * uint32_t* ID -> The page ID or address [not yet implemented]
  * Returns: The stored page's (24-bit) swap space ID/Address 
  */
-int store_page(void*, uint32_t*);
-// int store_page(void*, os_size_t, uint32_t*); To be implemented...
-uint32_t store_ext_page(void*, uint32_t*); // Waiting on file systems team to complete
-
+uint32_t *store_page(void*, uint32_t*);
+uint32_t *store_pageLZ(void*, uint32_t*); // Waiting on file systems team to complete
+// uint32_t *store_page(void*, os_size_t, uint32_t*); To be implemented...
 
 /* retrieve_page will retrieve the page identified the ID pointer and
  * will store it back into main memory (specified by the void *page pointer)
- * Returns: The retrieved page's (32-bit) new main memory ID/Address (like example)
- *
- * Note: Page size was set by store_page 
+ * Returns: The retrieved page's (32-bit) new main memory ID/Address (like above example)
+ * NOTE: Page size was set by store_page 
  */
 uint32_t retrieve_page(void*, uint32_t*);
-uint32_t retrieve_ext_page(void*, uint32_t*); // Waiting on file systems team to complete
+uint32_t retrieve_pageLZ(void*, uint32_t*); // Waiting on file systems team to complete
 
 
 // Returns: The total stored memory in bytes by function store_page(void*, uint32_t*)
@@ -84,33 +83,30 @@ os_size_t sum_stored();
  * and an auxillary swap spaces (specified by uint32_t *ID)
  * Returns: returns the new ID of where the swap_space was stored and changes the ID pointer 
  */
-uint32_t* vm_swap_page(void*, uint32_t*);
+uint32_t *vm_swap_page(void*, uint32_t*);
+uint32_t *vm_unswap_page(void*, uint32_t);
 
 
-/*
- *
- */
-//int vm_unswap_page(void*, uint32_t);
-
-
-/* vm_register/vm_deregister will activate/deactivate a swap space and set a priority (loewr is better)
- * accordingly to it. This allows the swap spaces use as well as increasing it's efficiency 
+/* vm_register/vm_deregister will activate/deactivate a swap space and set a priority
+ * accordingly to it. This allows the swap spaces use as well as increasing it's efficiency
+ * NOTE: deregister_swap_space will always deregister the lowest priority swap space first 
  * Returns: 0 or 1 whether the swap space registeration was a failure or success, respectively
  */
-int vm_register_swap_space(int (*)(void*, uint32_t*), int (*)(void*, uint32_t*), int);
-// As implied
-int vm_deregister_swap_space(int (*)(void*, uint32_t*), int (*)(void*, uint32_t*), int);
+uint8_t vm_register_swap_space(uint32_t (*)(void*, uint32_t*), uint32_t (*)
+		(void*, uint32_t*), int priority);
+uint8_t vm_deregister_swap_space();
 
 
-/*
- *
- * Returns: 
+/* Resolves the page fault and updates the corresponding page table
+ * Returns: The new resolved physical address of the page fault
+ * NOTE: May return a 0 if the page_fault could not be resolved
  */
 uint32_t vm_page_fault(void*);
 
-/*
- * Returns: 
+/* vm_scan_pages will scan through all pages for the corresponding page
+ * to the given page
+ * Returns: The address of matching swapped page 
  */
-uint32_t vm_scan_pages(void*,uint32_t*);
+uint32_t *vm_scan_pages(void*);
 
 #endif
