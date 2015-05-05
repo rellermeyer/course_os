a302604759cc5bb3b1c862420d844a19074d0908
Mathew Kurian <bluejamesbond@gmail.com>
2015-05-05 12:33:06 -0700

    Forgot to push hello

--- user/hello/hello.c
+++ user/hello/hello.c
@@ -9,26 +9,30 @@ uint32_t listener(uint32_t parent_tid) {
 
 int main(int argc, char ** argv) {
 
-	for (int i = 0; i < argc; i++) {
-		printf(argv[i], &argv[i]);
-		//	printf("\n", 99);
-	}
-
-	printf("Process ready\n", 99);
-
-	process_emit(12, 1, "Hey, how are you?", 18);
-
-	printf("Process started\n", 99);
-
-	process_emit(12, 1, "I am doing great!", 18);
-
-	printf("Process continued\n", 99);
-
-	process_emit(12, 1, "Are you sure?", 18);
-
-	process_yield();
-
-	process_emit(12, 1, "Yeah!!", 18);
+	__asm ("mov r0, #0x15":::);
+	__asm ("msr CPSR, r0":::);
+	__asm ("msr SPSR, r0":::);
+////
+////	for (int i = 0; i < argc; i++) {
+////		printf(argv[i], &argv[i]);
+////		//	printf("\n", 99);
+////	}
+//
+//	printf("Process ready\n", 99);
+//
+//	process_emit(12, 1, "Hey, how are you?", 18);
+//
+//	printf("Process started\n", 99);
+//
+//	process_emit(12, 1, "I am doing great!", 18);
+//
+//	printf("Process continued\n", 99);
+//
+//	process_emit(12, 1, "Are you sure?", 18);
+//
+//	process_yield();
+//
+//	process_emit(12, 1, "Yeah!!", 18);
 
 	while (1)
 		;
