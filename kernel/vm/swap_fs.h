c1c90bd03ff47d9b6b9bfb059e36b683ab73d81c
Noel Negusse <noel.blob@gmail.com>
2015-05-07 00:53:58 -0500

    Updated swapfs_retrieve() in swap_fs.c, added comments and updated header documentation

--- kernel/vm/swap_fs.h
+++ kernel/vm/swap_fs.h
@@ -1,27 +1,26 @@
 #ifndef __VM_SWAP_FS_H
 #define __VM_SWAP_FS_H
 
-#include "file.h"
 #include "bitvector.h"
-#include "vm.h"
-#include "klibc.h"
 #include "swap_framework.h"
-#include <stdio.h>
+
+
+/* Last Update: 05/06/15 */
 
 
 bit_vector *bv_arr[SWAP_SPACES];
-//bit_vector *free_bitvector;
+// bit_vector *free_bitvector;
 
 
 /* swapfs_init initializes swap file system  framework for 'npages' number of pages
  *
  * Returns: -1 if failure, number of bytes written on success
  */
-int32_t swapfs_init(int, int);
+int32_t swapfs_init(int, uint8_t);
 
 
-//Returns: -1 if failure, 1 if success
-int32_t swapfs_disable(int);
+/* Returns: -1 if failure, 1 if success */
+int32_t swapfs_disable(uint8_t);
 
 
 /* swapfs_store will store a page to media - not main memory - storage, e.g. HDD
@@ -30,16 +29,19 @@ int32_t swapfs_disable(int);
  * os_size_t pagesize: The size of a page in bytes, i.e. 4096 b
  * uint32_t* ID -> The page ID or address
  *
- * Returns: The stored page's swap space ID, index, -1 on failure
+ * Returns: The stored page's swap space ID, index OR -1 on failure
+ * NOTE: Relying on the changed id pointer is safer than using the output (unless for error checking)
+ * due to the int64_t type output
  */
-int32_t swapfs_store(void *, uint32_t *, int);
+int64_t swapfs_store(void*, uint32_t*, uint8_t);
 
 
-/* swapfs_retrieve will retrieve the page identified the ID pointer and
+/* swapfs_retrieve will retrieve the page identified by the ID pointer and
  * will store it back into main memory (specified by the void *page pointer)
  *
- * Returns: The retrieved page's memory ID/Address, -1 on failure
+ * Returns: -1 on failure and 1 on success
  */
-int32_t swapfs_retrieve(void *, uint32_t *);
+int32_t swapfs_retrieve(void*, uint32_t*, uint8_t);
+
 
 #endif
