9af56079997da1872ae90a9c283a947d7ca0fbe7
Noel Negusse <noel.blob@gmail.com>
2015-05-06 20:27:20 -0500

    Added a pqueue_find function in swap_pqueue.c and provided documentation in header file

--- kernel/vm/swap_pqueue.c
+++ kernel/vm/swap_pqueue.c
@@ -6,8 +6,7 @@
 void pqueue_init(struct swap_space *ss)
 { 
 	head = (struct node*) kmalloc(sizeof(struct node));
-	head->next = 0;
-	head->e_head = ss->e_head;
+	head->next = NULL;
 	head->lower_bits = ss->lower_bits;
 	head->priority = ss->priority;
 	head->store_func = ss->store_func;
@@ -16,19 +15,19 @@ void pqueue_init(struct swap_space *ss)
 	s = 1;
 }
 
-void pqueue_push(struct swap_space* ss)
+void pqueue_push(struct swap_space *ss)
 {
 	struct node *restore = path; int count=1;
 	path = head;
 	while(count<=pqueue_size()){
-		if(path->priority > ss->priority && count==1){ //set as new head
+		if (path->priority > ss->priority && count==1){ //set as new head
 			head = (struct node*) kmalloc(sizeof(struct node));
 			head->next = path;
 			pqueue_set(head, ss);
 			path = restore;
 			break;
 		}
-		if(path->priority < ss->priority && path->next == 0){ //set as new tail
+		if (path->priority < ss->priority && path->next == NULL){ //set as new tail
 			path = restore;
 			path->next = (struct node*) kmalloc(sizeof(struct node));
 			path = path->next;
@@ -36,7 +35,7 @@ void pqueue_push(struct swap_space* ss)
 			path->next = NULL;
 			break;
 		}
-		if(path->priority < ss->priority && count!=pqueue_size() && 
+		if (path->priority < ss->priority && count!=pqueue_size() && 
 				(path->next)->priority > ss->priority){ //set in between nodes
 			struct node *temp = (struct node*) kmalloc(sizeof(struct node));
 			pqueue_set(temp, ss);
@@ -65,14 +64,13 @@ void pqueue_pop_back()
 {
 	kfree(path);
 	path = head;
-	while(path->next == 0){
+	while(path->next == NULL){
 		path = path->next;
 	}
 	s--;
 }
 
 void pqueue_set(struct node *h, struct swap_space *ss){
-	h->e_head = ss->e_head;
 	h->lower_bits = ss->lower_bits;
 	h->priority = ss->priority;
 	h->store_func = ss->store_func;
@@ -81,10 +79,10 @@ void pqueue_set(struct node *h, struct swap_space *ss){
 
 struct node* pqueue_index(int i)
 {
-	if(i == 0){
+	if (i == 0){
 		return head;
 	}
-	if(i==(pqueue_size()-1)){
+	if (i == (pqueue_size()-1)){
 		return path;
 	}
 
@@ -92,6 +90,7 @@ struct node* pqueue_index(int i)
 	for(int x = 0; x<i; x++){
 		restore = restore->next;
 	}
+
 	return restore;
 }
 
@@ -109,4 +108,16 @@ uint32_t pqueue_peek(int type)
 	}
 }
 
+struct node* pqueue_find(uint8_t ssid)
+{
+	struct node *temp = head;
 
+	while(temp != NULL){
+		if (temp->lower_bits == ssid){
+			return temp;	 
+		}
+		temp = temp->next;
+	}
+
+	return NULL;
+}
