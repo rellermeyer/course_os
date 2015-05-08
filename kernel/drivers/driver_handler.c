d3fb874dd6ebabaee3867e97792d0b8945cfd139
Jorge Munoz <jorgmuno@cs.utexas.edu>
2015-05-08 18:40:18 -0500

    move ap to timer.c

--- kernel/drivers/driver_handler.c
+++ kernel/drivers/driver_handler.c
@@ -8,8 +8,10 @@ void function_call( void (*callback_function)(void *args),int handler)
 {
         function=callback_function;
 }
-
+void u_art_interrupt_hanadler(){
+}
 void timer_interrupt_handler(){
  	function(args);
         return;
 }
+
