d5f491f4618c040939a45c6bbe7f87219bfebac6
Jason <jsims1992@utexas.edu>
2015-05-07 23:00:09 -0500

    Readding malloc lost in previous revert: nothing else affected

--- user/libc/malloc/malloc_syscalls.c
+++ user/libc/malloc/malloc_syscalls.c
@@ -1,18 +1,20 @@
 #include <stdint.h>
-
-#include "../../include/malloc_syscalls.h"
+#include <malloc_syscalls.h>
 #include "../arch/arm/syscall_arch.h"
 #include "stdint.h"
 
-/*Malloc: Allocates a block of size bytes of memory. 
-Returns a pointer to the beggining of the block. If
-size <= 0, a null pointer is returned.*/
-
+/**
+ * malloc does a system call to allocate memory on the user heap
+ *
+ * @param  size of the block of memory allocated
+ * @param  uint32_t size > 0
+ * @return returns a pointer to the allocated block of memory
+ 		   returns 0 if size is less than 1
+ */ 
 void* malloc(uint32_t size)
 {
 	if (size <= 0)
 	{
-		os_printf("Inavlid size: must be greater than 0 bytes\n");
 		return 0;
 	}
 	//size will be passed into R0 for the syscall.
@@ -22,48 +24,48 @@ void* malloc(uint32_t size)
 	return (void*)retval;
 }
 
-/*calloc: Allocates memory for an array of num elements,
-each size bytes long, all zeroed out. Returns a pointer to 
-the beggining of the block. If size <= 0, a null pointer
-is returned.*/
-
-void* calloc(uint32_t num, uint32_t size)
+/**
+ * aligned alloc does a system call to allocate memory on the user heap
+ * according to a specified alignemnt
+ *
+ * @param  size of the block of memory allocated, and alignment desired
+ * @param  uint32_t size >0, uint32_t alignment >0
+ * @return returns a pointer to the allocated block of memory
+ * 		   that is a multiple of the specified allignement
+ *		   returns 0 if size or alignment are less than 1
+ */
+void* aligned_alloc(uint32_t size, uint32_t alignment)
 {
-	if (size <= 0 || num <= 0)
+	if (size <= 0 || alignment <= 0)
 	{
-		os_printf("Inavlid size and/or byte number: must be greater than 0\n");
 		return 0;
 	}
 	//syscall with two arguments
-	//num passed into R0, size passed into R1
-	long retval = __syscall2(SYSCALL_CALLOC, (long) num, (long) size);
+	//size passed into R0, alignment passed into R1
+	long retval = __syscall2(SYSCALL_ALIGNED_ALLOC, (long) size, (long) alignment);
 	//returns a pointer to begginning of allocated block(s)
 	return (void*) retval;
 }
 
-/*Free: Deallocates a block of memory previously
-allocated by Malloc, Calloc, etc.. */
-
+/**
+ * free does a system call to free an allocated block of memory on the heap
+ *
+ * @param  pointer to a block of memeory on the heap, does nothing if pointer is null
+ * @param  void* ptr
+ * @return nothing returned
+ */
 void free(void* ptr)
 {
 	//if null pointer, do nothing
 	if (ptr == 0)
 	{
-		os_printf("null pointer passed as parameter\n");
 		return 0;
 	}
 
 	//pointer of allocated block passed into R0
-	long retval = __syscall1(SYSCALL_MALLOC,(long)ptr);
-	
-	//negative retval for trying to free a block that doesnt exist
-	if (retval < 0)
-	{
-		os_printf("Memory you are trying to free has
-			not been previously allocated\n");
-	}
+	long retval = __syscall1(SYSCALL_FREE,(long)ptr);
 
-	//nothing returned for free
+	//nothing returned
 }
 
  
