c5349d4a88e1ffb1bf956619546332197b7ffab5
Vivian Roest <vivian@0x76.dev>
2020-02-27 22:25:07 +0100

    Some cleanup and transfered the vm test to our new testing framework.
    Co-authored-by: Jonathan Dönszelmann <jonabent@gmail.com>

--- kernel/src/ds/include/ds.h
+++ kernel/src/ds/include/ds.h
@@ -7,7 +7,6 @@
 // used across datastructures
 typedef void (*FreeFunc)(void * data);
 typedef bool (*CompareFunc)(void * in, void * other);
-typedef uint32_t (*HashFunc)(void * key);
 
 #include <vp_array_list.h>
 #include <vp_singly_linked_list.h>
