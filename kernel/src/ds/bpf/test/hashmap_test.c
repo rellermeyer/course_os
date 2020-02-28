f1c2d6da4f2e431337611f37a2d9f3bbf89a5a01
Vivian Roest <vivian@0x76.dev>
2020-02-28 11:50:47 +0100

    added some hashmap tests

--- kernel/src/ds/bpf/test/hashmap_test.c
+++ kernel/src/ds/bpf/test/hashmap_test.c
@@ -48,3 +48,97 @@ TEST_CREATE(test_free_entries_hm, {
 
     // Test framework will assert that we won't leak.
 })
+
+TEST_CREATE(test_find_hm, {
+    HashMap * hm = hashmap__new(int_hash_fn, int_compare_fn, fakeFree, fakeFree, NULL);
+
+    int key = 5;
+    int data = 42;
+
+    hashmap__add(hm, &key, &data);
+
+    ASSERT_EQ(hashmap__size(hm), 1);
+
+    int * ptr = NULL;
+    void ** dp = (void **) &ptr;
+
+    ASSERT(hashmap__find(hm, &key, dp));
+    ASSERT_EQ(&data, *dp);
+    ASSERT_EQ(data, *ptr);
+
+    hashmap__free(hm);
+})
+
+TEST_CREATE(test_set_hm, {
+    HashMap * hm = hashmap__new(int_hash_fn, int_compare_fn, fakeFree, fakeFree, NULL);
+
+    int key = 5;
+    int data = 42;
+
+    // add 42
+    hashmap__add(hm, &key, &data);
+
+    ASSERT_EQ(hashmap__size(hm), 1);
+
+    int * ptr = NULL;
+    void ** dp = (void **) &ptr;
+
+    ASSERT(hashmap__find(hm, &key, dp));
+    ASSERT_EQ(&data, *dp);
+    ASSERT_EQ(data, *ptr);
+
+    // set 42 to 69
+    int new_data = 69;
+    hashmap__set(hm, &key,&new_data, NULL, NULL);
+
+    ASSERT(hashmap__find(hm, &key, dp));
+    ASSERT_EQ(&new_data, *dp);
+    ASSERT_EQ(new_data, *ptr);
+
+    // set (144,196) which doesn't exist yet
+    int extra_data = 196;
+    int extra_key = 144;
+    hashmap__set(hm, &extra_key,&extra_data, NULL, NULL);
+
+    ASSERT(hashmap__find(hm, &extra_key, dp));
+    ASSERT_EQ(&extra_data, *dp);
+    ASSERT_EQ(extra_data, *ptr);
+
+    hashmap__free(hm);
+})
+
+
+TEST_CREATE(test_update_hm, {
+    HashMap * hm = hashmap__new(int_hash_fn, int_compare_fn, fakeFree, fakeFree, NULL);
+
+    int key = 5;
+    int data = 42;
+
+    // add 42
+    hashmap__add(hm, &key, &data);
+
+    ASSERT_EQ(hashmap__size(hm), 1);
+
+    int * ptr = NULL;
+    void ** dp = (void **) &ptr;
+
+    ASSERT(hashmap__find(hm, &key, dp));
+    ASSERT_EQ(&data, *dp);
+    ASSERT_EQ(data, *ptr);
+
+    // set 42 to 69
+    int new_data = 69;
+    hashmap__update(hm, &key,&new_data, NULL, NULL);
+
+    ASSERT(hashmap__find(hm, &key, dp));
+    ASSERT_EQ(&new_data, *dp);
+    ASSERT_EQ(new_data, *ptr);
+
+    // set (144,196) which doesn't exist
+    int extra_data = 196;
+    int extra_key = 144;
+    ASSERT_EQ(hashmap__update(hm, &extra_key,&extra_data, NULL, NULL), -1);
+
+    hashmap__free(hm);
+})
+
