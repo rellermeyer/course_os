a7eff4a04f9114d4ffb068260f2da145ffbd9df2
Victor Roest <victor@xirion.net>
2020-02-28 16:01:08 +0100

    moved kill to monitor script

--- kernel/src/test/monitor_tests.sh
+++ kernel/src/test/monitor_tests.sh
@@ -2,23 +2,25 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-set -x
+function quit_q() {
+    killall -9 qemu-system-arm; exit $1
+}
 
 tail -f -n +1 "$DIR/../../test.log" | while read -r line
 do
   if [[ $line =~ "TESTS COMPLETE" ]]; then
     echo "$line"
-    exit 0
+    quit_q 0
   elif [[ $line =~ "MEMORY LEAK" ]]; then
     echo "FAILED (MEMORY: $line)"
-    exit 1
+    quit_q 1
   elif [[ $line =~ "FAILED" ]]; then
     echo "FAILED"
-    exit 2
+    quit_q 2
   elif [[ $line =~ "DATA ABORT HANDLER" ]]; then
     echo "FAILED (EXCEPTION: $line)"
-    exit 3
+    quit_q 3
   else
-    echo "$line"
+    echo ">> $line"
   fi
 done
