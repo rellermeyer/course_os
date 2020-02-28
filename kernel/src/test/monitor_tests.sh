3258a66f1dae1abcb757b6194ec687c0204a21aa
jonay2000 <jonabent@gmail.com>
2020-02-28 15:39:10 +0100

    =~

--- kernel/src/test/monitor_tests.sh
+++ kernel/src/test/monitor_tests.sh
@@ -2,21 +2,23 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+set -x
+
 tail -f -n +1 "$DIR/../../test.log" | while read -r line
 do
-  if [[ $line == *"TESTS COMPLETE"* ]]; then
+  if [[ $line =~ "TESTS COMPLETE" ]]; then
     echo "$line"
     exit 0
-  elif [[ $line == *"MEMORY LEAK"* ]]; then
+  elif [[ $line =~ "MEMORY LEAK" ]]; then
     echo "FAILED (MEMORY: $line)"
     exit 1
-  elif [[ $line == *"FAILED"* ]]; then
+  elif [[ $line =~ "FAILED" ]]; then
     echo "FAILED"
     exit 2
-  elif [[ $line == *"DATA ABORT HANDLER"* ]]; then
+  elif [[ $line =~ "DATA ABORT HANDLER" ]]; then
     echo "FAILED (EXCEPTION: $line)"
     exit 3
   else
-    echo "$line"
+    echo ">>$line"
   fi
 done
