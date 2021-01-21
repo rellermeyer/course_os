c2aef7f032f2371070cd79320c6c4c629989cd16
jonay2000 <jonabent@gmail.com>
2021-01-21 20:19:02 +0100

    fix ci maybe probably I really really hope

--- config.mk
+++ config.mk
@@ -3,8 +3,8 @@ TOOLCHAIN_DIR=toolchain
 BARE_METAL_TUPLE=arm-none-eabi
 BARE_METAL_TARGET:=$(BARE_METAL_TUPLE)
 
-#QEMU=./qemu/qemu/bin/qemu-system-arm
-QEMU=/usr/bin/qemu-system-arm
+# use system qemu
+# QEMU=/usr/bin/qemu-system-arm
 
 #CFLAGS = -mcpu=arm1136j-s
 CFLAGS = -mcpu=arm1176jz-s
