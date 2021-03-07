55377d62e307e2ff77b9460da325f7a1db185f1c
zoharcochavi <zohar.cochavi@gmail.com>
2021-03-07 14:04:14 +0100

    Update documentation on context switching

--- src/kernel/src/sched/dispatcher.s
+++ src/kernel/src/sched/dispatcher.s
@@ -1,63 +1,77 @@
-.data
 .text
+
+// Subroutine definitions
+
 .global _save_state
 .global _load_state
 .global _switch_to_usermode
 .global _userspace_test_program
 
-.equ Mode_USR, 0x10 // User
+// Value to store in cpsr when you want to switch to user mode
+.equ Mode_USR, 0x10 
+
 /**
-    Saves the context in an ExecutionState at the location of the stack pointer of the calling process
-    Register contents on entry:
-    
-       R0 -> pc in user space, lr from interrupt 'space', argument so does not have to be stored
-       R1, R2 -> Scratch registers, do not have to be stored
-    
-       R4 -> SP_user_mode
-       R5 -> LR_user_mode
-    
-       R7 - R11 - just save
+    Save state will save the state of a process (execution state) onto the stack of said process, and return the address pointing to the top of the stack.
 
-       FP -> FP_user_mode
-       LR -> PC_user_mode (Our first argument)
-       SPSR -> CPSR_user_mode
+    It wil take 2 paramters:
+        r4: The current user stack pointer 
+        r0: The current user program counter 
+ 
+    Execution state:
+        The process state essentially consists of all the registers 
+        
+        - r4-r12 (general purpose registers)
+        - lr
+        - sp
+        - pc
 
-    It must be noted that a struct in C is interpreted as follows: 
-              the first element in the struct is the top element of the stack.
-              Since the stack is LIFO, we must add all enties in reverse order
- */
+    Return values:
+        The return value is the top of the user stack, including the execution state.
+
+    Note: Currently the context switcher only works for the SVC interrupt.
+          The code has to be rewritten to not require any parameters.
+        
+    Note: The return value is saved in r4 instead of r0. 
+          We use registers r0-r3 for the SYSCALL parameters. 
+          And we don't know which interrupt we're currently handling
+
+
+    An important thing to remember while reading this code is that there are certain registers that are 'banked'
+    when switching to a different mode. This means that they will not be overwritten when switching from one mode
+    to another. It is thus also important that we save the user banked registers (sp, and lr), and not those of the
+    current mode
+**/
 _save_state:
-    mov r1, sp                  // Save Interrupt stack in r1
+    mov r1, sp                  // Save current mode stack in r1
     mov sp, r4                  // Now relocate stack pointer to user stack (that way we can use macros such as push/pop)
 
-    push {r0} // pc
-    // Dump registers r6-r12, pc and lr onto the stack (Full Descending)
-    stmfd sp!, {r4 - r12}
+    push {r0}                   // Store program counter 
+    stmfd sp!, {r4 - r12}       // Store general purpose registers
+    // TODO: Save user banked LR onto the stack
 
-    mrs r2, spsr                // Load SPSR (cannot be immeadiatly loaded into memory)
-    push {r2}                   // Put it on the stack
+    mrs r2, spsr                // Load SPSR into a register (cannot be immeadiatly loaded into memory)
+    push {r2}                   // Store it
 
-    mov r4, sp                   // Save user space stack pointer back to r4
-    mov sp, r1                   // Restore Interrupt stack
-    bx lr                        // Return, remember that r4 is the pointer to the PCB
+    mov r4, sp                  // Save user space stack pointer back to r4
+    mov sp, r1                  // Restore current mode stack
+    bx lr                       // Return, remember that r4 is the pointer to the PCB
 
 /**
-    Loads and existing PCB, resuming execution of a process
+    Loads an existing Execution state, resuming execution of a process.
+    This is essentially the save state in reverse.
  */
 _load_state:
-    // TODO: Load existing PCB
     mov r1, sp                      // Save interrupt stack into r1 (never lose a reference)
     mov sp, r4                      // Set our stack pointer to user space
 
     pop {r2}                        
-    msr spsr_cxsf, r2                    // Load the program state
+    msr spsr_cxsf, r2               // Load the program state
     
-    ldmfd sp!, {r4 - r12}     // Load all user(^) general purpose registers, pc and lr
-    pop {lr}
+    ldmfd sp!, {r4 - r12}           // Load general purpose registers
+    pop {lr}                        // Load the user program counter into lr (will be stored into pc when returning)
 
-    mov sp, r1
-
-    movs pc, lr
+    mov sp, r1                      // Restore the current mode stack pointer
+    movs pc, lr                     // Set pc back to user program, and load the spsr into cpsr (the 's' in movs)
 
 /**
     Initializes a process with an zeroed PCB, and putting the Program Counter at the first instruction.
@@ -69,7 +83,6 @@ _init_state:
 
 _switch_to_usermode:
     MSR SPSR_c, #Mode_USR
-    // jump to the _userspace_test_program that for now just calls an empty software interrupt
     MOV r12, #0x8000
     add r12, #0x4
     MOVS pc, r12
@@ -83,11 +96,11 @@ _userspace_test_program:
     mov r1, #0x1
     mov r2, #0x2
     mov r3, #0x3
-    swi 0
+    svc 0
     mov r4, sp
     mov r0, #100
     mov r1, #0x1
     mov r2, #0x2
     mov r3, #0x3
-    swi 100
+    svc 100
 
