a835a1c4178d1391f756b9b2d214e9e0c5ae565a
Valentijn van de Beek <v.d.vandebeek@student.tudelft.nl>
2021-03-07 14:27:15 +0100

    Improve GDB emacs support

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,14 +1,17 @@
 ((nil . ((eval . (let ((root (projectile-project-root)))
-		   (let ((command (concat "arm-none-eabi-gdb -i=mi -ex \"target remote localhost:1234\" -ex \"symbol-file " root "src/kernel/build/kernel.sym\"")))
-                     (setq-local gud-gud-gdb-command-name
-				 command
-				 )
-		     (setq-local gud-gdb-command-name
-				 command
-				 )
-		     )
-		   )
-	       )
-	 )
-      )
- )
+                   (let ((command (concat "arm-none-eabi-gdb -i=mi -ex \"target remote localhost:1234\" -ex \"symbol-file " root "src/kernel/build/kernel.sym\"")))
+                     (setq-local gud-gud-gdb-command-name command)
+                     (setq-local gud-gdb-command-name command)
+                     (set (make-local-variable 'compile-command)
+                          (concat "cd " (concat root "src") " && make test"))
+                     (use-local-map (copy-keymap (current-local-map)))
+                     (local-set-key [f5] 'compile)
+                     (local-set-key [f6] 'co/gdb)
+
+                     (defun co/gdb ()
+                       (interactive)
+                       (async-shell-command (concat "cd "
+                                                    (concat (projectile-project-root) "src")
+                                                    " && "
+                                                    "make debug") nil 0)
+                       (gdb gud-gdb-command-name))))))))
