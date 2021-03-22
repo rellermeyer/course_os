219d2b20c099f6f6c3b963ede3f91bf00a0d3cb4
zoharcochavi <zohar.cochavi@gmail.com>
2021-03-22 10:57:34 +0100

    Fix r7 not being restored properly

--- src/kernel/src/common/interrupt_handler.s
+++ src/kernel/src/common/interrupt_handler.s
@@ -16,13 +16,16 @@
 // Disable interrupts? (IRQ, reset?)
 _handle_swi:
     // Put swi number in r7
+    push {r7}                   // Save r7
     ldr r7, [lr, #-4]           // SWI Instruction where we left off
     bic r7, r7, #0xff000000     // The value after the SWI instruction (swi call number)
     
     // Save lr when going into subroutine
     push {lr}
     bl syscall_handler 
-    pop {lr}
+    pop {lr} 
+
+    pop {r7}                    // Restore r7
     
     // Save the state of the process calling the software interrupt 
     _save_state_swi             
