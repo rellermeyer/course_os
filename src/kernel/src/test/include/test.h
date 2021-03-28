485c64c56cef638de5ef010ad54563c2bb7ac4aa
Valentijn van de Beek <v.d.vandebeek@student.tudelft.nl>
2021-03-28 17:10:35 +0200

    Rewrite unit tests; Configurable memory tracing; Memory leak detection fix

--- src/kernel/src/test/include/test.h
+++ src/kernel/src/test/include/test.h
@@ -2,75 +2,26 @@
 #ifndef TEST_H
 #define TEST_H
 
-#define TEST_PASS 1
-#define TEST_FAIL 0
-
 #include <allocator.h>
 #include <mem_alloc.h>
+#include <stdbool.h>
 #include <stdio.h>
+
+#define TEST_PASS true
+#define TEST_FAIL false
 
 // Only compile test definitions if enabled
 #ifdef ENABLE_TESTS
-
-__attribute__((__common__)) size_t global_counter;
-
-    #define DEBUG_COUNT kprintf("[DEBUG COUNT] %s:%i %i\n", __FILE__, __LINE__, global_counter++);
-
-    #ifdef MEM_DEBUG
-        #define TEST_CREATE(name, block)                                         \
-            int __internal_test_##name() {                                       \
-                heap_t * heap = mem_get_allocator();                             \
-                isize_t nbytes = heap->bytes_allocated;                          \
-                block;                                                           \
-                if (nbytes != (isize_t)heap->bytes_allocated) {                  \
-                    kprintf("\e[38;5;160mFAILED (MEMORY LEAK: %i bytes)\e[0m\n", \
-                            heap->bytes_allocated - nbytes);                     \
-                    return TEST_FAIL;                                            \
-                }                                                                \
-                return TEST_PASS;                                                \
-            }                                                                    \
-            int test_##name() {                                                  \
-                kprintf("\e[38;5;163m[TEST]\e[0m %s\n", #name);                  \
-                int res = __internal_test_##name();                              \
-                if (res == TEST_PASS) {                                          \
-                    kprintf("└─\e[92mPASSED\e[0m\n", #name);                     \
-                    return 1;                                                    \
-                } else {                                                         \
-                    kprintf("└─\e[38;5;160mFAILED\e[0m\n", #name);               \
-                    return 0;                                                    \
-                }                                                                \
-            }
-    #else
-        #define TEST_CREATE(name, block)                        \
-            int __internal_test_##name() {                      \
-                block;                                          \
-                return TEST_PASS;                               \
-            }                                                   \
-            int test_##name() {                                 \
-                kprintf("\e[38;5;163m[TEST]\e[0m %s\n", #name); \
-                int res = __internal_test_##name();             \
-                if (res == TEST_PASS) {                         \
-                    kprintf("└─\e[92mPASSED\e[0m\n");           \
-                    return 1;                                   \
-                } else {                                        \
-                    kprintf("└─\e[38;5;160mFAILED\e[0m\n");     \
-                    return 0;                                   \
-                }                                               \
-            }
-    #endif
-
-    #define PASS() return TEST_PASS
-    #define FAIL() return TEST_FAIL
+    #include "units.h"
     #include "assert.h"
 #else
 
     // If tests are disabled during compile time, all the macros expand to nothing
-
     #define TEST_CREATE(name, block)
     #define PASS()          0
     #define FAIL()          0
     #define ASSERT(expr)    0
-    /* #define ASSERT_EQ(l, r) 0 */
+    #define ASSERT_EQ(l, r) 0
 
 #endif
 
---
+++ src/kernel/src/test/include/units.h
@@ -0,0 +1,51 @@
+#ifndef UNITS_TEST_H
+#define UNITS_TEST_H
+#include <stdlib.h>
+#include <allocator.h>
+#include <stdbool.h>
+#include <ansi_escape.h>
+
+__attribute__((__common__)) size_t global_counter;
+
+#define DEBUG_COUNT kprintf("[DEBUG COUNT] %s:%i %i\n", __FILE__, __LINE__, global_counter++);
+
+#define TEST_CREATE(name, block)                                        \
+    bool run_test_##name() { block; return TEST_PASS; }                 \
+                                                                        \
+    bool __internal_test_##name() {                                     \
+        heap_t * heap = mem_get_allocator();                            \
+        isize_t nbytes = heap->bytes_allocated;                         \
+        bool res = run_test_##name();                                   \
+        if (nbytes != (isize_t)heap->bytes_allocated) {                 \
+            ANSI_256(160);                                              \
+            kprintf("FAILED (MEMORY LEAK: %i bytes)\n",                 \
+                    heap->bytes_allocated - nbytes);                    \
+            ANSI(RESET);                                                \
+            return TEST_FAIL;                                           \
+        }                                                               \
+        return res;                                                     \
+    }                                                                   \
+                                                                        \
+    bool test_##name() {                                                \
+        ANSI_256(163);                                                  \
+        puts("[TEST] ");                                                \
+        ANSI(RESET);                                                    \
+        kprintf("%s\n", #name);                                         \
+                                                                        \
+        bool res = (get_trace_memory()) ? __internal_test_##name() : run_test_##name(); \
+                                                                        \
+        if (res == TEST_PASS) {                                         \
+            ANSI(HI_GREEN);                                             \
+            kprintf("└─PASSED\n", #name);                               \
+            ANSI(RESET);                                                \
+            return 1;                                                   \
+        } else {                                                        \
+            ANSI(HI_RED);                                               \
+            kprintf("└─FAILED\n", #name);                               \
+            ANSI(RESET);                                                \
+            return 0;                                                   \
+        }                                                               \
+    }
+#define PASS() return TEST_PASS
+#define FAIL() return TEST_FAIL
+#endif
